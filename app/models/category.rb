31d54be894ba30a1c8563e4ffef6a697a6956c67
github username <github email address>
2018-03-26 16:53:26 -0400

    add has_many and has_many :through macros to Obsession and Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  has_many :obsession_categories
+  has_many :obsessions, through: :obsession_categories
 end
--- app/models/obsession.rb
+++ app/models/obsession.rb
@@ -1,3 +1,5 @@
 class Obsession < ApplicationRecord
   belongs_to :user
+  has_many :obsession_categories
+  has_many :categories, through: :obsession_categories
 end
