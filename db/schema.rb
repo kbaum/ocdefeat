69aa2dae305303b1ca7b9e02514d7755d6ca9de3
github username <github email address>
2018-03-26 16:55:50 -0400

    add test files and update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180326204238) do
+ActiveRecord::Schema.define(version: 20180326204543) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "obsession_categories", force: :cascade do |t|
+    t.integer "obsession_id"
+    t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
