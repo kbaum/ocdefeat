aafd7893f0431842b913f08dfab3216cb9506bad
github username <github email address>
2018-03-27 14:43:29 -0400

    comment out hash rocket syntax, rewrite shorter enum hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  enum role: { "unassigned_user" => 0, "patient" => 1, "therapist" => 2, "admin" => 3 }
+  enum role: { unassigned_user: 0, patient: 1, therapist: 2, admin: 3 }
+  #enum role: { "unassigned_user" => 0, "patient" => 1, "therapist" => 2, "admin" => 3 }
   has_secure_password
   has_many :obsessions
 end
