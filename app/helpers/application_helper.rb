434755419cdc6ef770427b66e19d0595f600ece2
github username <github email address>
2018-03-27 20:26:36 -0400

    add method set_class_for(flash_type) to ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def set_class_for(flash_type)
+    case flash_type
+    when "error"
+      "alert-danger"    # Red
+    when "alert"
+      "alert-warning"   # Yellow
+    when "notice"
+      "alert-info"      # Blue
+    end
+  end
+
 end
