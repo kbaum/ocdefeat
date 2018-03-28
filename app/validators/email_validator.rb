28a58e8788bbc72393e37c39f8cf33d4bba32153
github username <github email address>
2018-03-27 21:19:54 -0400

    add validate_each(record,attribute,value) to EmailValidator

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,3 +1,7 @@
-class EmailValidator
+class EmailValidator < ActiveModel::EachValidator
   EMAIL_REGEX = /\A[^@\s]+@[a-z\d\-.]+\.[a-z]{2,}\z/i
+
+  def validate_each(record, attribute, value)
+    record.errors[attribute] << (options[:message] || 'is invalid') unless value =~ EMAIL_REGEX
+  end
 end
