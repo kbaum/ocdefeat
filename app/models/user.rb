c9b967cda39e53e8480cecbe84518fa9352727c6
github username <github email address>
2018-03-27 21:02:01 -0400

    add dependent destroy to obsessions association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   enum role: { unassigned_user: 0, patient: 1, therapist: 2, admin: 3 }
   #enum role: { "unassigned_user" => 0, "patient" => 1, "therapist" => 2, "admin" => 3 }
   has_secure_password
-  has_many :obsessions
+  has_many :obsessions, dependent: :destroy
 end
