48c186e2a3ca79976f113a35039a9e0d0be5cae8
github username <github email address>
2018-03-27 21:06:25 -0400

    add validations for name and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
   enum role: { unassigned_user: 0, patient: 1, therapist: 2, admin: 3 }
   #enum role: { "unassigned_user" => 0, "patient" => 1, "therapist" => 2, "admin" => 3 }
-  has_secure_password
   has_many :obsessions, dependent: :destroy
   has_many :plans, through: :obsessions, dependent: :destroy
+
+  validates :name, presence: true
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 8 }, allow_nil: true
+  # when a user edits their user information, they don't have to retype their password
 end
