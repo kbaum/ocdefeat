f5cf8ebf11c0d3ffa9f2238038e9d13c63a30fbe
github username <github email address>
2018-03-27 20:32:24 -0400

    add user resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#home'
-  
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+
+  resources :users, only: [:show, :edit, :update, :destroy]
 end
