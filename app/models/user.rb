76b5224d08d86f94656f5187e14b26bf45ecbe90
github username <github email address>
2018-03-28 10:46:32 -0400

    add #find_or_create_by_omniauth(auth_hash) to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,18 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 8 }, allow_nil: true
   # when a user edits their user information, they don't have to retype their password
+  def self.find_or_create_by_omniauth(auth_hash)
+    self.where(email: auth_hash["info"]["email"]).first_or_create do |user|
+      user.name = auth_hash["info"]["name"]
+      user.password = SecureRandom.hex
+    end
+  end
 end
+
+# Explanation of #find_or_create_by_omniauth(auth_hash):
+# Trying to find user instance with the email provided by Facebook
+# If a user exists in users DB table with that email, return that user instance
+# If a user instance does NOT exist with that email, then create one with that Facebook email:
+# The newly instantiated user instance that was just created is passed to the block,
+# and we set its name attribute value = the name provided by Facebook -- auth_hash["info"]["name"]
+# and we give it a random, unique string password using SecureRandom.hex
