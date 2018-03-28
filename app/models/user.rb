988a71a67ce8c5f42f74f1dccd2367d6ff4506a0
github username <github email address>
2018-03-27 21:02:28 -0400

    add has_many plans association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   #enum role: { "unassigned_user" => 0, "patient" => 1, "therapist" => 2, "admin" => 3 }
   has_secure_password
   has_many :obsessions, dependent: :destroy
+  has_many :plans, through: :obsessions, dependent: :destroy
 end
