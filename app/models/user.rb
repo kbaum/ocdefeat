e4ff491a61ca602c9591a40533a3bb2b37e233b3
github username <github email address>
2018-03-29 13:34:09 -0400

    change find_or_create_by_omniauth(auth_hash) to have Twitter info

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,17 +10,21 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 8 }, allow_nil: true
   # when a user edits their user information, they don't have to retype their password
   def self.find_or_create_by_omniauth(auth_hash)
-    self.where(email: auth_hash["info"]["email"]).first_or_create do |user|
-      user.name = auth_hash["info"]["name"]
-      user.password = SecureRandom.hex
+    self.where(provider: auth_hash["provider"], uid: auth_hash["uid"]).first_or_create do |user|
+      user.name = auth_hash["info"]["name"] # "Jenna Leopold"
+      user.twitter_handle = auth_hash["info"]["nickname"] # "code_snippet_JL"
+      user.bio = auth_hash["info"]["description"] # "build code, break code, ad infinitum"
+      user.password = SecureRandom.hex # a random, unique string
     end
   end
 end
 
 # Explanation of #find_or_create_by_omniauth(auth_hash):
-# Trying to find user instance with the email provided by Facebook
-# If a user exists in users DB table with that email, return that user instance
-# If a user instance does NOT exist with that email, then create one with that Facebook email:
+# Trying to find user instance by their provider attribute ("Twitter") and their user ID on Twitter
+# If such a user exists in the DB already, return that user instance
+# If a user instance does NOT exist with that UID from Twitter, then create one:
 # The newly instantiated user instance that was just created is passed to the block,
-# and we set its name attribute value = the name provided by Facebook -- auth_hash["info"]["name"]
+# and we set its name attribute value = the name provided by Twitter -- auth_hash["info"]["name"],
+# we set its twitter_handle = auth_hash["info"]["nickname"],
+# we set its bio attribute = auth_hash["info"]["description"]
 # and we give it a random, unique string password using SecureRandom.hex
