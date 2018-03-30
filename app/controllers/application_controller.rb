13ce0478f32ec5192f8509ede8328065b04e1da4
github username <github email address>
2018-03-30 09:38:04 -0400

    define user_not_authorized(exception) for custom errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,12 @@ class ApplicationController < ActionController::Base
 
   private
 
+    def user_not_authorized(exception)
+      policy_name = exception.policy.class.to_s.underscore
+      flash[:alert] = t "#{policy_name}.#{exception.query}", scope: "pundit", default: :default
+      redirect_to(request.referrer || root_path)
+    end
+
     def current_user
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
