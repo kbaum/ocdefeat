62360480dc5e3c193e1f8593054f15b75bced612
github username <github email address>
2018-03-30 14:34:06 -0400

    define #set_user private method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,10 @@ class UsersController < ApplicationController
 
   private
 
+    def set_user
+      @user = User.find(params[:id])
+    end
+
     def user_params
       params.require(:user).permit(
         :name,
