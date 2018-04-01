3642d073d2c6d8de464d1da4ea55789a87383fa8
github username <github email address>
2018-04-01 14:41:08 -0400

    add users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,15 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id # log in the user
+      redirect_to user_path(@user), notice: "You successfully registered, #{current_user.name}! Thank you for joining the OCDefeat Community!"
+    else
+      flash.now[:error] = "Your registration attempt was unsuccessful. Please try again."
+      render :new # present the registration form so the user can try signing up again
+    end
   end
 
   def show
