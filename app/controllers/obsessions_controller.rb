d29bbb45f0aac1db938dc40400ed67e702e02a96
github username <github email address>
2018-04-02 17:44:11 -0400

    store all categories in @categories in obsessions#new

--- app/controllers/obsessions_controller.rb
+++ app/controllers/obsessions_controller.rb
@@ -1,6 +1,7 @@
 class ObsessionsController < ApplicationController
   def new
     @obsession = Obsession.new # @obsession instance for form_for to wrap around
+    @categories = Category.all # User can select which existing category their new obsession pertains to
   end
 
   def show
