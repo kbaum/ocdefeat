660810484817ee7de5dbe11b5079d1a3908d53d9
github username <github email address>
2018-04-04 18:01:35 -0400

    define #themes_attributes=(themes_attributes)

--- app/models/obsession.rb
+++ app/models/obsession.rb
@@ -11,4 +11,30 @@ class Obsession < ApplicationRecord
   validates :rituals, presence: true
 
   accepts_nested_attributes_for :themes, reject_if: proc { |attributes| attributes['name'].blank? }
+  # defines attribute writer method #themes_attributes= called on an obsession instance
+  # obsession record will not be built for the attributes hash if name of theme is blank
+  def themes_attributes=(themes_attributes)
+    themes_attributes.values.each do |theme_attribute|
+      if theme_attribute[:name].present?
+        theme = Theme.find_or_create_by(theme_attribute)
+        if !self.themes.include?(theme)
+          self.obsession_themes.build(:theme => theme)
+        end
+      end
+    end
+  end
+  # Explanation:
+  # themes_attributes is a hash that looks like {"name" => "Contamination OCD"}
+  # themes_attributes.values is the array ["Contamination OCD"]
+  # If the user submitted a name of an OCD theme in the form (in this case "Contamination OCD"),
+  # i.e. the name of the theme is present,
+  # find and return the theme instance whose name attribute value = "Contamination OCD" if one exists,
+  # but if such a theme does not exist, then create a theme instance with name = "Contamination OCD"
+  # If the obsession instance (self) is not already classified within that OCD theme,
+  # self.themes.include?(theme) returns false, and !false is true
+  # There is a many-to-many relationship between obsessions and themes, because an obsession can
+  # pertain to many OCD themes, and an OCD theme has many obsessions classified within it.
+  # An obsession_theme instance of ObsessionTheme join models belongs to both an obsession and a theme
+  # We're building an obsession_theme off of the obsession (self), so it already belongs to the obsession,
+  # and then we set the theme attribute of the obsession_theme instance = the theme instance
 end
