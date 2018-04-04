56a3684abc1cec0b0f7560019ed55c62e2aa986e
github username <github email address>
2018-04-04 15:59:31 -0400

    add validations for time_consumed, anxiety_rating

--- app/models/obsession.rb
+++ app/models/obsession.rb
@@ -3,4 +3,7 @@ class Obsession < ApplicationRecord
   has_many :obsession_themes
   has_many :themes, through: :obsession_themes, dependent: :destroy
   has_many :plans, dependent: :destroy
+
+  validates :time_consumed, presence: true, inclusion: { in: 0..24 }
+  validates :anxiety_rating, presence: true, inclusion: { in: 1..10 }
 end
