1e4d28a9c5c35bce516576f23eb5f56a5b6f74c7
github username <github email address>
2018-04-04 15:33:31 -0400

    add dependent: :destroy to has_many and has_many :through

--- app/models/obsession.rb
+++ app/models/obsession.rb
@@ -1,6 +1,6 @@
 class Obsession < ApplicationRecord
-  belongs_to :user
-  has_many :plans
+  belongs_to :user # obsessions table has user_id foreign key column
   has_many :obsession_themes
-  has_many :themes, through: :obsession_themes
+  has_many :themes, through: :obsession_themes, dependent: :destroy
+  has_many :plans, dependent: :destroy
 end
