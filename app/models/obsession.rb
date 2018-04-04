367d561c5bacb47d84e6790b3f22e8d9f24d2795
github username <github email address>
2018-04-04 16:00:27 -0400

    add validations for presence and uniqueness of intrusive_thought

--- app/models/obsession.rb
+++ app/models/obsession.rb
@@ -4,6 +4,7 @@ class Obsession < ApplicationRecord
   has_many :themes, through: :obsession_themes, dependent: :destroy
   has_many :plans, dependent: :destroy
 
+  validates :intrusive_thought, presence: true, uniqueness: true
   validates :time_consumed, presence: true, inclusion: { in: 0..24 }
   validates :anxiety_rating, presence: true, inclusion: { in: 1..10 }
 end
