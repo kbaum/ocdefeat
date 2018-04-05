12f35aaa51bec04ca75e9bb56a747594a1c93419
github username <github email address>
2018-04-04 20:04:55 -0400

    define private method #set_plan and create before_action

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,11 +1,13 @@
 class PlansController < ApplicationController
+  before_action :set_plan, only: [:show, :edit, :update, :destroy]
+
   def new
     @plan = Plan.new # instance for form_for to wrap around
   end
 
   def create
     @plan = Plan.new(plan_params)
-    
+
     if @plan.save
       redirect_to plan_path(@plan), notice: "You successfully created the ERP plan entitled #{@plan.title}!"
     else
@@ -24,6 +26,10 @@ class PlansController < ApplicationController
   end
 
   private
+
+    def set_plan
+      @plan = Plan.find(params[:id])
+    end
 
     def plan_params
       params.require(:plan).permit(
