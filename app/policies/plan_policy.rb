8f01349a609313050fbe2917300efe9f529715e2
github username <github email address>
2018-04-05 10:22:23 -0400

    add #new?,#create? permissions for patients to create plans

--- app/policies/plan_policy.rb
+++ app/policies/plan_policy.rb
@@ -10,4 +10,12 @@ class PlanPolicy < ApplicationPolicy
     # a plan belongs to an obsession, so a plan instance has obsession_id foreign key.
     # a user has many obsessions, so user.obsession_ids returns array of IDs of obsessions belonging to that user
   end
+
+  def new? # Only patients can view the form to create an ERP plan
+    user.patient?
+  end
+
+  def create? # Only patients can create a preliminary plan, including title and goal
+    user.patient?
+  end
 end
