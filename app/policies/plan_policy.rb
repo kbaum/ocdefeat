03bd141e5a49c26468447b32524215f453ca297c
github username <github email address>
2018-04-05 10:17:49 -0400

    add permissions to view plans index

--- app/policies/plan_policy.rb
+++ app/policies/plan_policy.rb
@@ -1,2 +1,13 @@
 class PlanPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      if user.therapist? || user.admin? # Therapists and admins can see the index of all ERP plans
+        scope.all
+      elsif user.patient? # A patient can only view an index of HIS OWN plans
+        scope.where("obsession_id IN (?)", user.obsession_ids)
+      end
+    end
+    # a plan belongs to an obsession, so a plan instance has obsession_id foreign key.
+    # a user has many obsessions, so user.obsession_ids returns array of IDs of obsessions belonging to that user
+  end
 end
