20de307cb7775169e75c16ea63da4ab126247986
github username <github email address>
2018-04-08 19:48:55 -0400

    define #div_class_for_step(step) in StepsHelper

--- app/helpers/steps_helper.rb
+++ app/helpers/steps_helper.rb
@@ -1,2 +1,12 @@
 module StepsHelper
+
+  def div_class_for_step(step)
+    "completed" if step.complete?
+  end
+
 end
+
+# Explanation of #div_class_for_step(step):
+# Calling #complete? on step instance returns true if status attribute value of step instance = 1
+# If the step is complete (i.e. its status attribute = 1), then method returns the string "completed"
+# which becomes the class attribute value of the div -- <div class="completed">
