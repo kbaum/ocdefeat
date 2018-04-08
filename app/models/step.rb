8addafbad878d5d8d669241702c8f92fd359d53d
github username <github email address>
2018-04-08 16:21:02 -0400

    define #complete? and #incomplete? instance methods

--- app/models/step.rb
+++ app/models/step.rb
@@ -3,6 +3,14 @@ class Step < ApplicationRecord
     :incomplete => 0,
     :complete => 1
   }
-  
+
   belongs_to :plan
+
+  def complete?
+    self.status == STEP_STATUS[:complete]
+  end
+
+  def incomplete?
+    self.status == STEP_STATUS[:incomplete]
+  end
 end
