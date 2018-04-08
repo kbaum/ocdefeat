7fcdaf8aa7d0389431de83e6419ac96c6ad9a1e6
github username <github email address>
2018-04-08 16:25:59 -0400

    add validations for plan's instructions and duration

--- app/models/step.rb
+++ app/models/step.rb
@@ -4,13 +4,15 @@ class Step < ApplicationRecord
     :complete => 1
   }
 
-  belongs_to :plan
+  belongs_to :plan # therefore, steps table has plan_id foreign key column
+  validates :instructions, presence: true
+  validates :duration, presence: true # duration is a string, such as "1 hour", "15 minutes"
 
-  def complete?
+  def complete? # a completed step (self) has a status attribute value = 1
     self.status == STEP_STATUS[:complete]
   end
 
-  def incomplete?
+  def incomplete? # an incomplete step (self) has a status attribute value = 0
     self.status == STEP_STATUS[:incomplete]
   end
 end
