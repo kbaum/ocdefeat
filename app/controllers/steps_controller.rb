ac79cff2dbe3ba81ac01ac062493173b6fcd4508
github username <github email address>
2018-04-08 20:13:52 -0400

    remove steps#new, define steps#create

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,5 +1,14 @@
 class StepsController < ApplicationController
-  def new
+
+  def create
+    @step = Step.new(step_params)
+    authorize @step
+    if @step.save
+      redirect_to plan_path(@step.plan), notice: "A new step has been added to this ERP plan!"
+    else
+      flash.now[:error] = "Your attempt to add a new step to this ERP plan was unsuccessful. Please try again."
+      render "plans/show"
+    end
   end
 
   def edit
