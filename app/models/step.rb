0736ca887bd5b4bd9b4cbad9a811115523d7e9a1
github username <github email address>
2018-04-09 16:33:35 -0400

    add <br>  before validations, add discomfort_degree validation

--- app/models/step.rb
+++ app/models/step.rb
@@ -5,8 +5,10 @@ class Step < ApplicationRecord
   }
 
   belongs_to :plan # therefore, steps table has plan_id foreign key column
+  
   validates :instructions, presence: true
   validates :duration, presence: true # duration is a string, such as "1 hour", "15 minutes"
+  validates :discomfort_degree, inclusion: { in: 1..10 }
 
   def complete? # a completed step (self) has a status attribute value = 1
     self.status == STEP_STATUS[:complete]
