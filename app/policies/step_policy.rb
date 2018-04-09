4abad3e00ff811674955ced6a3dd0bd4aaab51e6
github username <github email address>
2018-04-09 18:52:22 -0400

    define private method #step_owner in StepPolicy

--- app/policies/step_policy.rb
+++ app/policies/step_policy.rb
@@ -2,4 +2,10 @@ class StepPolicy < ApplicationPolicy # A step does not exist outside the context
   def create? # patient who owns plan to which the step belongs, and any therapist, can create a new step for that plan
     user.patient? || user.therapist?
   end
+
+  private
+
+  def step_owner
+    user == record.plan.designer
+  end
 end
