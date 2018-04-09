3a3387b5ae84e93029329d732c715f2b8d3176bc
github username <github email address>
2018-04-09 16:52:13 -0400

    define StepPolicy class with permissions to create step

---
+++ app/policies/step_policy.rb
@@ -0,0 +1,5 @@
+class StepPolicy < ApplicationPolicy # A step does not exist outside the context of an ERP plan
+  def create? # patient who owns plan to which the step belongs, and any therapist, can create a new step for that plan
+    user.patient? || user.therapist?
+  end
+end
