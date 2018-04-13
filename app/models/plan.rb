884beeb0293dae7ac74ecc020ff9b93ec54a42ac
github username <github email address>
2018-04-13 09:47:11 -0400

    define #done? instance method in Plan model

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -8,4 +8,8 @@ class Plan < ApplicationRecord
   def designer
     self.obsession.user
   end
+
+  def done? # returns true if plan consists of at least 1 step and all steps are completed (step's status = 1)
+    self.steps.count > 0 && self.steps.all? {|step| step.complete?}
+  end
 end
