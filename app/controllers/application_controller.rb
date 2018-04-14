daf957a2bbb8224a63969d99d46abd406f5c4c12
github username <github email address>
2018-04-14 17:22:06 -0400

    allow privacy and terms actions to skip login_required

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-  before_action :login_required, except: [:new, :create, :home]
+  before_action :login_required, except: [:new, :create, :home, :privacy, :terms]
 
   private
 
