22d73fa94a79e245fa23e06753ea2050f9684b5c
github username <github email address>
2018-04-15 11:58:05 -0400

    add validation for email presence remove allow_blank: true

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
   has_many :plans, through: :obsessions, dependent: :destroy
 
   validates :name, presence: true
-  validates :email, email: true, uniqueness: true, allow_blank: true
-  # I only want to validate the email attribute of a user instance if it's present
+  validates :email, presence: true, email: true, uniqueness: true
+  
   has_secure_password
   validates :password, presence: true, length: { minimum: 8 }, allow_nil: true
   # when a user edits their user information, they don't have to retype their password
