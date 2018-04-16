48ef1cc8560d14440708f6d80baa5ceb801b6242
github username <github email address>
2018-04-16 19:06:45 -0400

    change severity of seed data instances to 'Nonobsessive'

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ admin.name = "Adelaide Adminski"
 admin.email = "admin@ocdefeat.com"
 admin.password = "ocdefeat"
 admin.role = 3
-admin.severity = "not_applicable"
+admin.severity = "Nonobsessive"
 admin.save
 
 therapist = User.new
@@ -11,5 +11,5 @@ therapist.name = "Dr. Simon Therapinski"
 therapist.email = "psychologist@ocdefeat.com"
 therapist.password = "psychology"
 therapist.role = 2
-therapist.severity = "not_applicable"
+therapist.severity = "Nonobsessive"
 therapist.save
