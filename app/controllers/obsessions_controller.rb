e97aa6620c7562ba7a55438bb4fbbcb61fe65c77
github username <github email address>
2018-04-20 19:19:54 -0400

    add logic to #count_obsessions when filterer is admin, change flash alert msgs on redirect

--- app/controllers/obsessions_controller.rb
+++ app/controllers/obsessions_controller.rb
@@ -53,10 +53,12 @@ class ObsessionsController < ApplicationController
 
     def count_obsessions # this method is called before #index action
       obsessions = policy_scope(Obsession)
-      if current_user.therapist? && obsessions.empty?
-        redirect_to user_path(current_user), alert: "Your patients have made progress...the obsessions log is currently empty!"
+      if current_user.admin? && obsessions.empty?
+        redirect_to user_path(current_user), alert: "The obsessions log is currently empty!"
+      elsif current_user.therapist? && obsessions.empty?
+        redirect_to user_path(current_user), alert: "None of your patients are currently obsessing, and all past obsessions have been defeated and deleted!"
       elsif current_user.patient? && current_user.obsessions.empty?
-        redirect_to user_path(current_user), alert: "Looks like you haven't been obsessing lately! No obsessions were found!"
+        redirect_to user_path(current_user), alert: "You're making progress! Looks like you haven't been obsessing lately! No obsessions were found."
       end
     end
 
