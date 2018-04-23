7c52a318f5a6a96f7a6d1d985c8d50d7f0ae3ce1
github username <github email address>
2018-04-23 14:29:33 -0400

    add validation for name uniqueness in Theme model

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,6 +1,7 @@
 class Theme < ApplicationRecord
   has_many :obsession_themes
   has_many :obsessions, through: :obsession_themes
+  validates :name, uniqueness: true
 
   def num_obsessions
     self.obsessions.count > 0 ? "This OCD theme currently contains #{pluralize(self.obsessions.count, 'obsession')}." :
