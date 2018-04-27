592748ccc9b731fb21ee9bccca924c389505797f
github username <github email address>
2018-04-27 15:21:00 -0400

    delete num_obsessions instance method from Theme model

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -2,9 +2,4 @@ class Theme < ApplicationRecord
   has_many :obsession_themes
   has_many :obsessions, through: :obsession_themes
   validates :name, uniqueness: true
-
-  def num_obsessions
-    self.obsessions.count > 0 ? "This OCD theme currently contains #{pluralize(self.obsessions.count, 'obsession')}." :
-    "No obsessions currently pertain to this OCD theme!"
-  end
 end
