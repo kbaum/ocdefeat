054ae94441060e59513dd7dbf83202a4dfa0fd0a
github username <github email address>
2018-04-28 19:32:56 -0400

    delete obsessions#index; add logic for patient filtering by theme in obsessions#index and no filter

--- app/controllers/obsessions_controller.rb
+++ app/controllers/obsessions_controller.rb
@@ -7,78 +7,20 @@ class ObsessionsController < ApplicationController
     @patients = User.where(role: 1)
     @themes = Theme.all
 
-    if current_user.admin?
-      if !params[:num_plans].blank? # Admin filters obsessions by number of ERP plans
-        if params[:num_plans] == "Least to Most ERP Plans"
-          @obsessions = obsessions.least_to_most_desensitized
-        else
-          @obsessions = obsessions.most_to_least_desensitized
-        end
-      elsif !params[:date].blank? # Admin filters obsessions by date created
-        if params[:date] == "Today"
-          @obsessions = obsessions.from_today
-        else
-          @obsessions = obsessions.old_obsessions
-        end
-      else # Admin did not choose a filter, so all patients' obsessions are listed
-        @obsessions = obsessions
-      end
-    elsif current_user.therapist?
-      if !params[:patient].blank? # Therapist filters obsessions by patient -- params[:patient] is the ID of the patient selected by name from dropdown
-        if @patients.find(params[:patient]).obsessions.empty? # if the selected patient has no obsessions
-          redirect_to obsessions_path, alert: "That patient currently has no obsessions!"
-        else
-          @obsessions = obsessions.by_patient(params[:patient])
-        end
-      elsif !params[:distressed].blank? # Therapist filters obsessions by a patient's most distressing obsession -- params[:distressed] is the ID of the user selected to find that user's most distressing obsession
-        obsession = Obsession.most_distressing_by_user(params[:distressed]) # obsession will either be nil or the user's obsession instance with the highest anxiety_rating attribute value
+    if current_user.patient?
+      if !params[:ocd_theme].blank? # Patient filters her own obsessions by OCD theme - params[:ocd_theme] is the ID of the theme in which the obsessions we're searching for are categorized
+        theme_name = Theme.find(params[:ocd_theme]).name
+        themed_obsessions = obsessions.by_theme(params[:ocd_theme])
 
-        if obsession.nil? # the selected user has no obsessions
-          redirect_to obsessions_path, alert: "That patient currently has no obsessions, so no obsession is deemed most distressing!"
+        if themed_obsessions.empty? # If none of the patient's obsessions pertain to that theme
+          @obsessions = nil
+          flash.now[:alert] = "None of your obsessions pertain to \"#{theme_name}.\""
         else
-          @obsession = obsession
+          @obsessions = themed_obsessions # stores 'array' of the patient's obsessions categorized in the selected OCD theme
+          flash.now[:notice] = "At least one of your obsessions is categorized as \"#{theme_name}!\""
         end
-      elsif !params[:consumed].blank? # Therapist filters obsessions by a patient's most time-consuming obsession -- params[:consumed] is the ID of the user selected to find that user's most time-consuming obsession
-        obsession = Obsession.most_time_consuming_by_user(params[:consumed])
-
-        if obsession.nil? # the selected user has no obsessions
-          redirect_to obsessions_path, alert: "That patient currently has no obsessions, so no obsession is deemed most time-consuming!"
-        else
-          @obsession = obsession
-        end
-      elsif !params[:ocd_subset].blank? # Therapist filters obsessions by OCD subset
-        @obsessions = obsessions.by_theme(params[:ocd_subset])
-        if @obsessions.empty?
-          redirect_to obsessions_path, alert: "No obsessions pertain to this OCD subset."
-        else
-          @obsessions
-        end
-      else # Therapist did not choose a filter, so all patients' obsessions are listed
-        @obsessions = obsessions
-      end
-    elsif current_user.patient?
-      if !params[:ocd_theme].blank? # Patient filtering her own obsessions by OCD theme
-        @obsessions = obsessions.by_theme(params[:ocd_theme])
-
-        if @obsessions.empty?
-          redirect_to obsessions_path, alert: "No obsessions pertain to this OCD theme."
-        else
-          @obsessions
-        end
-      elsif !params[:anxiety_amount].blank? # Patient filtering her own obsessions by anxiety_rating
-        if params[:anxiety_amount] == "Least to Most Distressing"
-          @obsessions = obsessions.least_to_most_distressing
-        else
-          @obsessions = obsessions.most_to_least_distressing
-        end
-      elsif !params[:time_taken].blank? # Patient filtering her own obsessions by time_consumed
-        if params[:time_taken] == "Least to Most Time-Consuming"
-          @obsessions = obsessions.least_to_most_time_consuming
-        else
-          @obsessions = obsessions.most_to_least_time_consuming
-        end
-      else # Patient did not choose a filter, so all of her own obsessions are listed
-        @obsessions = obsessions
+      else # Patient did not choose a filter
+        @obsessions = obsessions # stores all of the patient's own obsessions
       end
     end
   end
