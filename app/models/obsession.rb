c4952883909e34c26d1baea9fd582804f4996f6f
github username <github email address>
2018-04-30 17:00:51 -0400

    define class method least_to_most_plans in Obsession model

--- app/models/obsession.rb
+++ app/models/obsession.rb
@@ -88,6 +88,10 @@ class Obsession < ApplicationRecord
     self.find_each.reject {|o| o.plans_per_obsession > 0}
   end
 
+  def self.least_to_most_plans
+    self.all.sort_by {|o| o.plans_per_obsession}
+  end
+
   def obsessify(thought) # instance method called on obsession instance takes string argument, sets intrusive_thought attribute of obsession to formatted string
     self.intrusive_thought = "What if I " << "#{thought}?"
   end
