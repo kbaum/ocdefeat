8a8766bb5b0e7ab1d31aa09ce0e933b39bcae88b
github username <github email address>
2018-05-01 13:46:15 -0400

    define method,before_action require_themes in ThemesController; make themes local var in #index

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,5 +1,16 @@
 class ThemesController < ApplicationController
+  before_action :require_themes, only: :index
+
   def index
-    @themes = policy_scope(Theme)
+    themes = policy_scope(Theme)
   end
+
+  private
+
+    def require_themes # private filter method called before themes#index
+      themes = policy_scope(Theme)
+      if themes.empty? # If there are no OCD themes
+        redirect_to root_url, alert: "The Index of OCD Themes is currently empty!"
+      end
+    end
 end
