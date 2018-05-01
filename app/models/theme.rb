036f65e0899074d2bd64574e3dc85797268644e9
github username <github email address>
2018-05-01 14:12:19 -0400

    specify unique users in comment about #prevalence_in_patients

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -3,7 +3,7 @@ class Theme < ApplicationRecord
   has_many :obsessions, through: :obsession_themes
   validates :name, uniqueness: true
 
-  def prevalence_in_patients # instance method returns number of users who have at least 1 obsession w/ theme content
+  def prevalence_in_patients # instance method returns number of unique users who have at least 1 obsession w/ theme content
     self.obsessions.map {|o| o.user}.uniq.count
   end
 
