1be3d122a7a9f62ea2e6b85111aeffbf6a818145
github username <github email address>
2018-05-01 13:37:17 -0400

    define instance method #prevalence_in_patients in Theme model

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -2,4 +2,8 @@ class Theme < ApplicationRecord
   has_many :obsession_themes
   has_many :obsessions, through: :obsession_themes
   validates :name, uniqueness: true
+
+  def prevalence_in_patients # instance method returns number of users who have at least 1 obsession w/ theme content
+    self.obsessions.map {|o| o.user}.uniq.count
+  end
 end
