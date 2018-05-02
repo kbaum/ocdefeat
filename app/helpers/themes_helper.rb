153e8f5895818e9054e57f79199ae4aa110816e9
github username <github email address>
2018-05-02 12:24:47 -0400

    define helper method #tally_popularity(theme) in ThemesHelper

--- app/helpers/themes_helper.rb
+++ app/helpers/themes_helper.rb
@@ -1,2 +1,14 @@
 module ThemesHelper
+  def tally_popularity(theme)
+    if theme.obsessions_per_theme == 0
+      "No patients are currently obsessing about \"#{theme.name}.\""
+    else
+      <<-HEREDOC
+      #{theme.prevalence_in_patients} distinct #{'patient'.pluralize(theme.prevalence_in_patients)}
+      #{'is'.pluralize(theme.prevalence_in_patients)} currently obsessing about #{theme.name}, in which
+      #{theme.obsessions_per_theme} #{'obsession'.pluralize(theme.obsessions_per_theme)}
+      #{'is'.pluralize(theme.obsessions_per_theme)} classified.
+      HEREDOC
+    end
+  end
 end
