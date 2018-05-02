a9e0fe842c1dc7916e6252c0b39d9d6fd1ba2c80
github username <github email address>
2018-05-02 16:10:03 -0400

    delete 'distinct' and 'currently' in #tally_popularity(theme) heredoc

--- app/helpers/themes_helper.rb
+++ app/helpers/themes_helper.rb
@@ -4,8 +4,8 @@ module ThemesHelper
       "No patients are currently obsessing about \"#{theme.name}.\""
     else
       <<-HEREDOC
-      #{theme.prevalence_in_patients} distinct #{'patient'.pluralize(theme.prevalence_in_patients)}
-      #{'is'.pluralize(theme.prevalence_in_patients)} currently obsessing about #{theme.name}, in which
+      #{theme.prevalence_in_patients} #{'patient'.pluralize(theme.prevalence_in_patients)}
+      #{'is'.pluralize(theme.prevalence_in_patients)} obsessing about #{theme.name}, in which
       #{theme.obsessions_per_theme} #{'obsession'.pluralize(theme.obsessions_per_theme)}
       #{'is'.pluralize(theme.obsessions_per_theme)} classified.
       HEREDOC
