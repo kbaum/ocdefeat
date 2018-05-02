c3e3dfe7e2a16bb63a1bdb105f63365c0ed017dd
github username <github email address>
2018-05-02 12:18:55 -0400

    delete helper method num_obsessions_for(theme) from ThemesHelper

--- app/helpers/themes_helper.rb
+++ app/helpers/themes_helper.rb
@@ -1,6 +1,2 @@
 module ThemesHelper
-  def num_obsessions_for(theme)
-    theme.obsessions.count > 0 ? "This OCD theme currently contains #{pluralize(theme.obsessions.count, 'obsession')}." :
-    "No obsessions currently pertain to this OCD theme!"
-  end
 end
