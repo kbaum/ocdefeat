585c31f8d7569e4a8cc8c3ff6aeeb70e59cc2118
github username <github email address>
2018-05-11 15:07:13 -0400

    add validation to ensure name is not blank in Theme model

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,7 +1,7 @@
 class Theme < ApplicationRecord
   has_many :obsession_themes
   has_many :obsessions, through: :obsession_themes, dependent: :destroy
-  validates :name, uniqueness: true
+  validates :name, presence: true, uniqueness: true
 
   def prevalence_in_patients # instance method returns number of unique users who have at least 1 obsession w/ theme content
     self.obsessions.map {|o| o.user}.uniq.count
