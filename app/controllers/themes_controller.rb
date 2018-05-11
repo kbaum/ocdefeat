cc506f1595be80a77cc9fc8f8bea67dd2a998f67
github username <github email address>
2018-05-11 18:22:49 -0400

    change flash[:alert] to flash.now[:alert] in #require_themes

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -69,7 +69,7 @@ class ThemesController < ApplicationController
     def require_themes # private method called before themes#index ensures that there is at least 1 existing theme when patient/admin views index
       if Theme.all.empty? # If there are no OCD themes
         if current_user.therapist?
-          flash[:alert] = "The Index of OCD Themes is currently empty. Add a new theme below!"
+        flash.now[:alert] = "The Index of OCD Themes is currently empty. Add a new theme below!"
         else
           redirect_to root_url, alert: "The Index of OCD Themes is currently empty."
         end
