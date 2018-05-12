922347d441eda7c4d22155a834d61692d6b19cee
github username <github email address>
2018-05-12 15:39:28 -0400

    define #status_of(plan) helper method in PlansHelper

--- app/helpers/plans_helper.rb
+++ app/helpers/plans_helper.rb
@@ -1,2 +1,11 @@
 module PlansHelper
+
+  def status_of(plan)
+    if plan.steps.empty?
+      "Development Phase (must add steps)"
+    else
+      plan.done? ? "Finished! (Fully performed and desensitization achieved)" : "Unfinished (must execute all ERP exercises)"
+    end
+  end
+
 end
