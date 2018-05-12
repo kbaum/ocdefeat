30a60013790a81dc151fd87b7ab1962dd41efae3
github username <github email address>
2018-05-12 14:43:24 -0400

    delete logic in plans#index for params[:title] filter

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -9,10 +9,7 @@ class PlansController < ApplicationController
     @themes = policy_scope(Theme)
 
     if current_user.patient?
-      if !params[:title].blank? # Patient filters her own plans by title -- params[:title] stores unique title of plan
-        @plans = plans # need patient's own plans for select_tag in app/views/filter_plans/_patient.html.erb
-        @plan = current_user.plans.by_title(params[:title])
-      elsif !params[:obsession_targeted].blank? # Patient filters her own plans by the obsession targeted -- params[:obsession_targeted] is the ID of obsession for which the patient searches plans
+      if !params[:obsession_targeted].blank? # Patient filters her own plans by the obsession targeted -- params[:obsession_targeted] is the ID of obsession for which the patient searches plans
         thought = current_user.obsessions.find(params[:obsession_targeted]).intrusive_thought
         @plans = current_user.obsessions.find(params[:obsession_targeted]).plans
         if @plans.empty? # If no plans for that obsession were found
