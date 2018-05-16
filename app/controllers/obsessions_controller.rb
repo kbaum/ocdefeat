88bfc480124341a5368ec4f62a2374561a5047a8
github username <github email address>
2018-05-16 18:34:24 -0400

    change 'that lack' to 'lacking' in flash msg for params[:planless] logic

--- app/controllers/obsessions_controller.rb
+++ app/controllers/obsessions_controller.rb
@@ -111,7 +111,7 @@ class ObsessionsController < ApplicationController
           flash.now[:alert] = "Patient #{patient_picked.name} diligently designed ERP plans for every obsession."
         else # If the patient has obsessions for which no ERP plans were designed
           @obsessions = patient_picked.obsessions.sans_plans
-          flash.now[:notice] = "Patient #{patient_picked.name} has #{plural_inflection(@obsessions)} that lack ERP plans!"
+          flash.now[:notice] = "Patient #{patient_picked.name} has #{plural_inflection(@obsessions)} lacking ERP plans!"
         end
       elsif !params[:ocd_subset].blank? # Therapist filters obsessions by OCD subset -- params[:ocd_subset] is the ID of the theme
         string_subset = Theme.find(params[:ocd_subset]).name
