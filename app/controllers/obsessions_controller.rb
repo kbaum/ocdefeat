5ccbf27a43892a7f131ae8a347d75011be4bab30
github username <github email address>
2018-05-16 16:35:52 -0400

    delete logic for params[:distressed] in obsessions#index

--- app/controllers/obsessions_controller.rb
+++ app/controllers/obsessions_controller.rb
@@ -67,24 +67,7 @@ class ObsessionsController < ApplicationController
           @obsessions = obsessions.by_patient(params[:patient]) # stores AR::Relation of all the selected patient's obsessions
           flash.now[:notice] = "Patient #{@patients.find(params[:patient]).name} has #{plural_inflection(@obsessions)}!"
         end
-      elsif !params[:distressed].blank? # Therapist filters obsessions by a patient's obsessions ordered by descending distress degree (i.e. from highest to lowest anxiety_rating)
-        patient_picked = @patients.find(params[:distressed]) # params[:distressed] is the ID of the user whose obsessions we're ordering by descending distress degree
-        if patient_picked.obsessions.empty? # If the selected patient has no obsessions
-          flash.now[:alert] = "#{patient_picked.name} is not distressed, as this patient is not obsessing about anything!"
-        else # The selected patient has obsessions
-          first_rating = patient_picked.obsessions.first.anxiety_rating
-          if patient_picked.obsession_count == 1 # If the selected patient only has one obsession
-            @obsessions = patient_picked.obsessions
-            flash.now[:notice] = "Patient #{patient_picked.name} only has one obsession rated at anxiety level #{first_rating}!"
-          else # If the selected patient has more than 1 obsession
-            if patient_picked.obsessions.all? {|o| o.anxiety_rating == first_rating} # all of the selected patient's obsessions have the same anxiety_rating, so none are displayed
-              flash.now[:alert] = "#{patient_picked.name}'s obsessions cannot be ordered by descending degree of distress, as this patient rated each obsession at anxiety level #{first_rating}."
-            else # patient has multiple obsessions that do not all have the same anxiety_rating
-              @obsessions = patient_picked.obsessions.most_to_least_distressing # stores 'array' of all the selected patient's obsessions ordered by descending distress degree
-              flash.now[:notice] = "#{patient_picked.name}'s obsessions are ordered from most to least distressing, so you can prioritize treating the obsessions that bring this patient the most discomfort!"
-            end
-          end
-        end
+      elsif !params[:distressed].blank? # Therapist filters obsessions by a patient's obsessions ordered from highest to lowest anxiety_rating
       elsif !params[:consumed].blank? # Therapist filters obsessions by a patient's obsessions ordered from most to least time-consuming
         patient_picked = @patients.find(params[:consumed]) # params[:consumed] is the ID of the user whose obsessions we're ordering from most to least time-consuming
         if patient_picked.obsessions.empty? # If the selected patient has no obsessions
