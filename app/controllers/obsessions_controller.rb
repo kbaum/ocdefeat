6e1b0698975da6d3f5c76cf8e639d3c6dbe9eb15
github username <github email address>
2018-05-16 19:00:01 -0400

    add flash.now[:notice] to obsessions#index when therapist doesn't filter

--- app/controllers/obsessions_controller.rb
+++ app/controllers/obsessions_controller.rb
@@ -122,6 +122,7 @@ class ObsessionsController < ApplicationController
         end
       else # Therapist did not select a filter
         @obsessions = obsessions # stores all patients' obsessions
+        flash.now[:notice] = "All patients' obsessions are recorded below!"
       end
     elsif current_user.admin?
       if !params[:plan_productivity].blank? # Admin filters obsessions by number of ERP plans per obsession
