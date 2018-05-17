9a11a95eeafff8cbcae2b498ff2297ac8c7971ab
github username <github email address>
2018-05-17 19:16:49 -0400

    delete steps#update; call set_step_and_parent_plan before show,destroy

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,5 +1,5 @@
 class StepsController < ApplicationController
-  before_action :set_step_and_parent_plan, only: [:edit, :update, :destroy]
+  before_action :set_step_and_parent_plan, only: [:show, :destroy]
 
   def create # POST request to "/plans/:plan_id/steps" maps to steps#create
     @step = Step.new(step_params) # new step will automatically belong to plan b/c plan_id is stored in params
@@ -14,21 +14,6 @@ class StepsController < ApplicationController
 
   def edit # GET request to "/plans/:plan_id/steps/:id/edit" maps to steps#edit
     authorize @step
-  end
-
-  def update # PATCH request to "/plans/:plan_id/steps/:id" maps to steps#update
-    authorize @step
-
-    if @step.incomplete? # If the step is incomplete (status = 0) before changes are made
-      if @step.update(status: params[:step][:status]) == @step.complete? # And if the step is updated from incomplete to complete
-        redirect_to plan_path(@plan), notice: "Milestone accomplished! You're one step closer to defeating OCD!"
-      end
-    elsif @step.update(step_params)
-      redirect_to plan_path(@plan), notice: "You successfully modified a step in this ERP plan!"
-    else
-      flash.now[:error] = "Your attempt to edit this ERP exercise was unsuccessful. Please try again."
-      render :edit
-    end
   end
 
   def destroy # deleting a step - DELETE request to "/plans/:plan_id/steps/:id" mapped to steps#destroy
