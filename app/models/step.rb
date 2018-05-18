556c76d39f7d3c4bd29543f813e57b79647e265d
github username <github email address>
2018-05-17 20:40:27 -0400

    add allow_blank: true to discomfort_degree validation in Step model

--- app/models/step.rb
+++ app/models/step.rb
@@ -3,12 +3,12 @@ class Step < ApplicationRecord
     :incomplete => 0,
     :complete => 1
   }
-  
+
   belongs_to :plan # therefore, steps table has plan_id foreign key column
 
   validates :instructions, presence: true
   validates :duration, presence: true # duration is a string, such as "1 hour", "15 minutes"
-  validates :discomfort_degree, inclusion: { in: 1..10 }, on: :update
+  validates :discomfort_degree, inclusion: { in: 1..10 }, on: :update, allow_blank: true
 
   def complete? # a completed step (self) has a status attribute value = 1
     self.status == STEP_STATUS[:complete]
