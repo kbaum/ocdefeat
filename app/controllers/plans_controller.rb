96c3b244bed3158bb922d3c74b7c39c71f679040
github username <github email address>
2018-05-20 14:32:21 -0400

    change 'assessing' to 'determining' in alert msg for params[:completion]

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -108,7 +108,7 @@ class PlansController < ApplicationController
         end
       elsif !params[:completion].blank? # Admin filters plans by finished/unfinished plans
         if plans.procedural.empty? # If NO plans with at least 1 step were found (i.e. all plans have no steps)
-          flash.now[:alert] = "ERP plans must have at least one step before assessing status of completion."
+          flash.now[:alert] = "ERP plans must have at least one step before determining status of completion."
         else # Plans with at least 1 step were found
           if params[:completion] == "Finished"
             if plans.finished.empty? # If no finished plans were found
