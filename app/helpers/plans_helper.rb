5ee89d86f36cd124d20eb0b30b5a50a4b832f02d
github username <github email address>
2018-05-21 15:39:31 -0400

    change Development Phase to In Development in status_of(plan)

--- app/helpers/plans_helper.rb
+++ app/helpers/plans_helper.rb
@@ -2,7 +2,7 @@ module PlansHelper
 
   def status_of(plan)
     if plan.steps.empty?
-      "Development Phase (must add steps)"
+      "In Development (must add steps)"
     else
       plan.done? ? "Finished! (Fully performed and desensitization achieved)" : "Unfinished (must execute all ERP exercises)"
     end
