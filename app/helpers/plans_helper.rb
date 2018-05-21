1d3bd9b62a5b358677fb125b5d4fd1ffa72172b3
github username <github email address>
2018-05-21 12:29:11 -0400

    define #report(finished, unfinished) in PlansHelper

--- app/helpers/plans_helper.rb
+++ app/helpers/plans_helper.rb
@@ -12,4 +12,14 @@ module PlansHelper
     finished.blank? ? "#{unfinished.first.designer.name}" : "#{finished.first.designer.name}"
   end
 
+  def report(finished, unfinished)
+    if finished && unfinished # If the patient has both finished AND unfinished plans
+      "finished #{finished.to_ary.count} ERP #{'plan'.pluralize(finished.to_ary.count)} and left #{unfinished.count} ERP #{'plan'.pluralize(unfinished.count)} unfinished."
+    elsif finished.nil? # If the patient did NOT finish any ERP plans
+      "failed to finish any ERP plans and left #{unfinished.count} ERP #{'plan'.pluralize(unfinished.count)} unfinished."
+    elsif unfinished.nil? # If the patient only has finished ERP plans
+      "achieved desensitization by implementing #{finished.to_ary.count} ERP #{'plan'.pluralize(finished.to_ary.count)} from start to finish!"
+    end
+  end
+
 end
