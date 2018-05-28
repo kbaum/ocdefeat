ea3b75193e2dbe6ef5dd33e7b4516212b893d398
github username <github email address>
2018-05-28 12:22:49 -0400

    define #obsession_pronouns and #plan_pronouns helpers

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,17 @@
 module UsersHelper
+  def obsession_pronouns
+    if current_user.patient?
+      "You are"
+    elsif current_user.therapist?
+      "The patient is"
+    end
+  end
+
+  def plan_pronouns
+    if current_user.patient?
+      "You have designed"
+    elsif current_user.therapist?
+      "The patient has designed"
+    end
+  end
 end
