82646df8ace150f8670efa83c48cb13360437f5a
github username <github email address>
2018-05-29 13:11:09 -0400

    define #select_severity(user) and #vary_variant(user) in UsersHelper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -14,4 +14,20 @@ module UsersHelper
       "The patient has designed"
     end
   end
+
+  def select_severity(user)
+    if current_user.patient?
+      "Your role was recently changed to patient. Do you have mild, moderate, severe or extreme OCD?"
+    elsif current_user.therapist? || current_user.admin?
+      "Now that #{user.name}'s role was changed, this patient must report a valid OCD severity."
+    end
+  end
+
+  def vary_variant(user)
+    if current_user.patient?
+      "Your role was recently changed to patient. Were you diagnosed with Traditional OCD, Pure-O or both variants of the disorder?"
+    elsif current_user.therapist? || current_user.admin?
+      "Now that #{user.name}'s role was changed, this patient must report a valid OCD variant."
+    end
+  end
 end
