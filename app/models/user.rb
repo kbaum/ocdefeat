d264f1aaa6c12aa85c3b2b8b6997ad6424cc74c9
github username <github email address>
2018-05-30 12:03:55 -0400

    define scope :unassigned_users in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  scope :unassigned_users, -> { where(role: 0) }
   scope :admins, -> { where(role: 3) }
   scope :therapists, -> { where(role: 2) }
   scope :patients, -> { where(role: 1) }
