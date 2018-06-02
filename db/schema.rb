89db8d0265c94fa651599d1714ac9dba47f3201d
github username <github email address>
2018-06-02 18:04:28 -0400

    generate and run migration to create notes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180602220157) do
+ActiveRecord::Schema.define(version: 20180602220333) do
+
+  create_table "notes", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "obsession_themes", force: :cascade do |t|
     t.integer "obsession_id"
