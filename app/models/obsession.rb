d75b8f56b5c7d6104b5c3b1b1ee5889362f4073e
github username <github email address>
2018-06-02 14:54:56 -0400

    add variant method to delegate macro in Obsession model

--- app/models/obsession.rb
+++ app/models/obsession.rb
@@ -5,7 +5,7 @@ class Obsession < ApplicationRecord
   scope :symptomless, -> { where(symptoms: ["", " "]) }
 
   belongs_to :user # obsessions table has user_id foreign key column
-  delegate :name, to: :user, prefix: :patient # I can call #patient_name on obsession instance to return the name attribute value of user instance to which obsession belongs
+  delegate :name, :variant, to: :user, prefix: :patient # I can call #patient_name on obsession instance to return the name attribute value of user instance to which obsession belongs. I can also call #patient_variant on obsession to return OCD variant of user to which obsession belongs
   has_many :obsession_themes
   has_many :themes, through: :obsession_themes, dependent: :destroy
   has_many :plans, dependent: :destroy
