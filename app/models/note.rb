93b9465b86eee7e4aebe1bf965358b1d83adfd6b
github username <github email address>
2018-06-02 18:13:11 -0400

    add belongs_to :user and delegate macros in Note model

---
+++ app/models/note.rb
@@ -0,0 +1,4 @@
+class Note < ApplicationRecord
+  belongs_to :user
+  delegate :name, to: :user, prefix: true, allow_nil: true # calling #user_name on note instance returns the name attribute value of the user instance to which the note belongs.
+end
