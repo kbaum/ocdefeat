94e2588909884fdb7a9b5f1a486e02596cbc7190
github username <github email address>
2018-06-02 15:32:07 -0400

    add delegate macro to call #therapist_name on tip instance

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -1,3 +1,4 @@
 class Tip < ApplicationRecord
   belongs_to :user
+  delegate :name, to: :user, prefix: :therapist # I can call #therapist_name on tip instance to return the name attribute value of user instance to which tip belongs.
 end
