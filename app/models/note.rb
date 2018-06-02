c10227bfaaa01fe52cefa4cc07c62a1e7a28627a
github username <github email address>
2018-06-02 15:27:36 -0400

    delete Note model, rollback migration to create notes table

--- app/models/note.rb
+++
@@ -1,4 +0,0 @@
-class Note < ApplicationRecord
-  belongs_to :user
-  delegate :name, to: :user, prefix: :therapist # I can call #therapist_name on note instance to return the name attribute value of the user instance to which the note belongs.
-end
