8600f34218514180b376feb4f60fb82edad424a7
github username <github email address>
2018-06-02 18:20:07 -0400

    add user_id foreign key column to notes table in migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180602220333) do
+ActiveRecord::Schema.define(version: 20180602221922) do
 
   create_table "notes", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "obsession_themes", force: :cascade do |t|
