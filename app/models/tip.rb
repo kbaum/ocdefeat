07f11e33efa24f98af1c83c6d39f786d883376c7
github username <github email address>
2018-06-02 18:14:02 -0400

    delete Tip model

--- app/models/tip.rb
+++
@@ -1,4 +0,0 @@
-class Tip < ApplicationRecord
-  belongs_to :user
-  delegate :name, to: :user, prefix: :therapist # I can call #therapist_name on tip instance to return the name attribute value of user instance to which tip belongs.
-end
