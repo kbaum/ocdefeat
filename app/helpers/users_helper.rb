7b70b6bf88c33bf49a98467053b050c047d4b753
github username <github email address>
2018-06-02 19:30:31 -0400

    define #label_note helper in UsersHelper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -63,4 +63,12 @@ module UsersHelper
       render partial: "users_ul", locals: { users: User.by_ocd_variant(variant) }
     end
   end
+
+  def label_note
+    if current_user.therapist?
+      "Give this patient some therapy pointers:"
+    elsif current_user.patient?
+      "Voice your concerns to a counselor:"
+    end
+  end
 end
