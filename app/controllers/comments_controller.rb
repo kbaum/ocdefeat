b3aef54bf2854f495730b7dae23a6a61dc5d914e
github username <github email address>
2018-06-04 16:38:46 -0400

    edit comments#create; add comment_params returning strong params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,17 @@
 class CommentsController < ApplicationController
   def create
-    binding.pry
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to obsession_path(@comment.obsession)
+    else
+      flash.now[:error] = "Your attempt to comment on this obsession was unsuccessful. Please try again."
+      render "obsessions/show"
+    end
   end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:content, :obsession_id)
+    end
 end
