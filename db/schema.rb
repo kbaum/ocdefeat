8c86133b1c256bae05bd4ebd111ca7134228f9b3
github username <github email address>
2018-06-04 14:28:24 -0400

    add obsession_id foreign key to comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180604182051) do
+ActiveRecord::Schema.define(version: 20180604182735) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "obsession_id"
   end
 
   create_table "obsession_themes", force: :cascade do |t|
