f42dc7c2c10642c334324a4bc5ec50dcd6532f15
github username <github email address>
2018-06-04 14:17:18 -0400

    run migration to drop notes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180604174925) do
-
-  create_table "notes", force: :cascade do |t|
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "user_id"
-    t.integer "obsession_id"
-  end
+ActiveRecord::Schema.define(version: 20180604181620) do
 
   create_table "obsession_themes", force: :cascade do |t|
     t.integer "obsession_id"
