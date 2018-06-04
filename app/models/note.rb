8f4b0000754a46a1819067d636d2ab1550978efe
github username <github email address>
2018-06-04 14:20:24 -0400

    delete Note model

--- app/models/note.rb
+++
@@ -1,5 +0,0 @@
-class Note < ApplicationRecord
-  belongs_to :user
-  delegate :name, to: :user, prefix: true, allow_nil: true # calling #user_name on note instance returns the name attribute value of the user instance to which the note belongs.
-  validates :content, presence: true
-end
