70ba31f4da60342cff4399ce1a4132462cfaa8cc
github username <github email address>
2018-06-04 17:56:52 -0400

    define #rhyme_symptoms(obsession) in ObsessionsHelper

--- app/helpers/obsessions_helper.rb
+++ app/helpers/obsessions_helper.rb
@@ -2,4 +2,12 @@ module ObsessionsHelper
   def originated_on(obsession)
     obsession.created_at.strftime("%A, %B %e, %Y, at %l:%M %p")
   end
+
+  def rhyme_symptoms(obsession)
+    if obsession.symptoms.blank?
+      "until every last fear evaporates"
+    else
+      ", and symptoms such as #{obsession.symptoms} will dissipate"
+    end
+  end
 end
