5a4a39d2fa744e56977e3145e5397e93b792eab4
github username <github email address>
2018-06-04 14:31:25 -0400

    validate presence of content in Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :obsession
   belongs_to :user
   delegate :name, to: :user, prefix: true, allow_nil: true # calling #user_name on comment instance returns the name attribute value of the user instance to which the comment belongs.
+  validates :content, presence: true
 end
