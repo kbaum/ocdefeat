aabd2f1b48777248566bbfcb4efc8546bb04867b
github username <github email address>
2018-06-05 08:13:58 -0400

    define #display_obsessions(obsessions) in ObsessionsHelper

--- app/helpers/obsessions_helper.rb
+++ app/helpers/obsessions_helper.rb
@@ -1,4 +1,14 @@
 module ObsessionsHelper
+  def display_obsessions(obsessions)
+    unless obsessions.nil?
+      content_tag(:ul) do
+        obsessions.each do |obsession|
+          concat(content_tag(:li, link_to_unless(current_user.admin?, obsession.intrusive_thought, obsession_path(obsession))))
+        end
+      end
+    end
+  end
+
   def originated_on(obsession)
     obsession.created_at.strftime("%A, %B %e, %Y, at %l:%M %p")
   end
