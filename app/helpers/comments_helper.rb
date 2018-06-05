ab0a10d847df1ee651a08573b5422f6e4ac1a6fa
github username <github email address>
2018-06-05 08:32:31 -0400

    change 'this' to 'the' in #label_comment helper

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,7 +1,7 @@
 module CommentsHelper
   def label_comment
     if current_user.therapist?
-      "Give this patient some therapy pointers:"
+      "Give the patient some therapy pointers:"
     elsif current_user.patient?
       "Voice your concerns to a counselor:"
     end
