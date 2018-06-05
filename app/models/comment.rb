9d2fc0f530e3a54da6d3c8af3585dac83bce1414
github username <github email address>
2018-06-05 11:29:27 -0400

    define scope :patient_complaints in Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
 class Comment < ApplicationRecord
   scope :counseling_comments, -> { where(user_id: User.therapists) }
+  scope :patient_complaints, -> { where(user_id: User.patients) }
+  
   belongs_to :obsession
   belongs_to :user
   delegate :name, to: :user, prefix: true, allow_nil: true # calling #user_name on comment instance returns the name attribute value of the user instance to which the comment belongs.
