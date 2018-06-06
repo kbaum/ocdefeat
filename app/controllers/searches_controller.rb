b4c3bd578af9a06f07861070307dd585867e4d7b
github username <github email address>
2018-06-06 17:41:57 -0400

    define search_params strong_params in SearchesController

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,11 +4,24 @@ class SearchesController < ApplicationController
   end
 
   def create
-    @search = Search.create!(params[:search])
-    redirect_to search_path(@search), notice: "Your search results are displayed below."
+    @search = Search.new(search_params)
+    if @search.save
+      redirect_to search_path(@search), notice: "Your search results are displayed below."
+    end
   end
 
   def show
     @search = Search.find(params[:id])
   end
+
+  private
+
+    def search_params
+      params.require(:search).permit(
+        :key_terms,
+        :user_id,
+        :min_anxiety_rating,
+        :max_anxiety_rating
+      )
+    end
 end
