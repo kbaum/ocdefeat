fc0ac8e588867361fd07ceee152e9bf2a1957575
github username <github email address>
2018-06-06 17:45:47 -0400

    change theme_id to user_id in Search model

--- app/models/search.rb
+++ app/models/search.rb
@@ -8,7 +8,7 @@ class Search < ApplicationRecord
     def obtain_obsessions
       obsessions = Obsession.order(:intrusive_thought)
       obsessions = obsessions.where("intrusive_thought like ?", "%#{key_terms}%") if key_terms.present?
-      obsessions = obsessions.where(theme_id: theme_id) if theme_id.present?
+      obsessions = obsessions.where(user_id: user_id) if user_id.present?
       obsessions = obsessions.where("anxiety_rating >= ?", min_anxiety_rating) if min_anxiety_rating.present?
       obsessions = obsessions.where("anxiety_rating <= ?", max_anxiety_rating) if max_anxiety_rating.present?
       obsessions
