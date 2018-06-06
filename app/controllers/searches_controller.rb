29122e58e119cbf153484b8d609afbcba1d91a04
github username <github email address>
2018-06-06 15:58:36 -0400

    define searches#create and searches#show

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,4 +2,13 @@ class SearchesController < ApplicationController
   def new
     @search = Search.new
   end
+
+  def create
+    @search = Search.create!(params[:search])
+    redirect_to search_path(@search), notice: "Your search results are displayed below."
+  end
+
+  def show
+    @search = Search.find(params[:id])
+  end
 end
