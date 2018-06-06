f3802e360285d38fd4d8ead51931ab7ccbd60fb0
github username <github email address>
2018-06-05 20:40:43 -0400

    define comments#index; change 'thanks' to 'thank you' in #comment_creation_msg

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,6 +12,26 @@ class CommentsController < ApplicationController
   def index # Route helper obsession_comments_path returns "/obsessions/:obsession_id/comments", which maps to comments#index
     @obsession = Obsession.find(params[:obsession_id])
     @comments = @obsession.comments
+    @user = @obsession.user
+    authorize @user, :show_comments?
+
+    if !params[:type].blank? # If the user filters comments by type
+      if params[:type] == "Patient Concerns"
+        if @obsession.comments.concerns.empty?
+          flash.now[:alert] = "#{commenter} voiced no concerns about this obsession."
+        else
+          @comments = @obsession.comments.concerns
+          flash.now[:notice] = "#{commenter} voiced #{@comments.count} #{'concern'.pluralize(@comments.count)} about this obsession."
+        end
+      elsif params[:type] == "Advice from Therapists"
+        if @obsession.comments.advice.empty?
+          flash.now[:alert] = "Unfortunately, no therapy pointers were given to #{commenter.downcase}."
+        else
+          @comments = @obsession.comments.advice
+          flash.now[:notice]= "#{commenter} should bear #{@comments.count} therapy #{'pointer'.pluralize(@comments.count)} in mind when struggling with this obsession."
+        end
+      end
+    end
   end
 
   private
@@ -22,9 +42,17 @@ class CommentsController < ApplicationController
 
     def comment_creation_msg
       if current_user.therapist?
-        "Thanks for sharing your advice!"
+        "Thank you for sharing your advice!"
       elsif current_user.patient?
-        "Thanks for reaching out to our therapy team so we can effectively address your concerns!"
+        "Thank you for reaching out to our therapy team so we can effectively address your concerns!"
+      end
+    end
+
+    def commenter
+      if current_user.patient?
+        "You"
+      elsif current_user.therapist?
+        "The patient"
       end
     end
 end
