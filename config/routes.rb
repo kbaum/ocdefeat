21626bbc2a178593f48856db4200404ecf3b3979
github username <github email address>
2018-06-07 17:31:01 -0400

    add :users resources back in routing file

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,13 @@ Rails.application.routes.draw do
     get :part1
     get :part2
     get :part3
-
     post :validate_part
-    # Routes generated:
-    # get "/wizard/part1" => wizards#part1
-    # get "/wizard/part2" => wizards#part2
-    # get "/wizard/part3" => wizards#part3
-    # post "/wizard/validate_part" => wizards#validate_part
   end
+  # Explanation of routes generated above:
+  # #part1_wizard_path returns GET "/wizard/part1", which maps to wizards#part1
+  # part2_wizard_path returns GET "/wizard/part2", which maps to wizards#part2
+  # part3_wizard_path returns GET "/wizard/part3", which maps to wizards#part3
+  # validate_part_wizard_path returns POST "/wizard/validate_part", which maps to wizards#validate_part
 
   #get '/signup' => 'users#new'
   #post '/users' => 'users#create'
@@ -20,7 +19,7 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
 
-  #resources :users, only: [:index, :show, :edit, :update, :destroy]
+  resources :users, only: [:index, :show, :edit, :update, :destroy]
 
   resources :obsessions do
     resources :comments, only: [:create, :index, :edit, :update, :destroy]
