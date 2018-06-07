ea84dfadf0046018e2af66ba80b7505248bdd6b2
github username <github email address>
2018-06-07 16:08:21 -0400

    change 'current' to 'present'; stub out wizards#create

--- app/controllers/wizards_controller.rb
+++ app/controllers/wizards_controller.rb
@@ -1,6 +1,6 @@
 class WizardsController < ApplicationController
   before_action :instantiate_current_user_part, only: [:part1, :part2, :part3, :create]
-  
+
   def part1 # implicitly renders app/views/wizards/part1.html.erb (1st part of signup form)
   end
 
@@ -10,14 +10,17 @@ class WizardsController < ApplicationController
   def part3 # implicitly renders app/views/wizards/part3.html.erb (3rd part of signup form)
   end
 
+  def create
+  end
+
   private
     def instantiate_user_part(string_part) # valid argument = "part1", "part2" or "part3"
       raise InvalidPart unless string_part.in?(Wizard::User::PARTS)
       "Wizard::User::#{part.camelize}".constantize.new(session[:user_properties])
     end
 
-    def instantiate_current_user_part
-      @current_user_part = instantiate_user_part(action_name)
+    def instantiate_present_user_part
+      @present_user_part = instantiate_user_part(action_name)
     end
 
     class InvalidPart < StandardError; end
@@ -41,3 +44,12 @@ end
 # stores an instance of Wizard::User::Part1 or Wizard::User::Part2 or Wizard::User::Part3,
 # (which has a hash of the user's attribute data stored in session hash),
 # depending on whatever the current part is
+
+# validate_part explanation:
+# Every time the user clicks the button to go to the next part of the multipart signup form,
+# the method #validate_part is called.
+# validate_part either redirects to the next part of the signup form,
+# or, in the case of invalid user input, renders the current part again to display errors to the user
+# Each time #validate_part is called when the user validates a part,
+# we store all of the attributes of the user instance as a hash
+# rather than storing the instance itself in the session hash)
