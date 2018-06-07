a0399ef53bd6029d69bd3b2caf17869b7de85369
github username <github email address>
2018-06-07 14:34:42 -0400

    define #instantiate_user_part(string_part) in WizardsController

--- app/controllers/wizards_controller.rb
+++ app/controllers/wizards_controller.rb
@@ -9,6 +9,17 @@ class WizardsController < ApplicationController
   end
 
   private
+    def instantiate_user_part(string_part) # valid argument = "part1", "part2" or "part3"
+      raise InvalidPart unless string_part.in?(Wizard::User::PARTS)
+      "Wizard::User::#{part.camelize}".constantize.new(session[:user_properties])
+    end
 
     class InvalidPart < StandardError; end
+
 end
+# "part1".camelize returns "Part1", "part2".camelize returns "Part2", "part3".camelize returns "Part3"
+# constantize tries to find a constant (in this case, the name of a class)
+# named "Wizard::User::Part1" or "Wizard::User::Part2" or "Wizard::User::Part3"
+# (with constantize, a NameError is raised when the name is not in CamelCase or the constant is unknown)
+# We instantiate an instance of Wizard::User::Part1 or Wizard::User::Part2 or Wizard::User::Part3,
+# passing in a hash of attributes of the AR user instance who's signing up
