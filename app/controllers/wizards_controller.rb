a1c74f602848094c3065f4d0ed35cf5c867e2170
github username <github email address>
2018-06-07 13:50:28 -0400

    generate WizardsController w/ part1, part2, part3 actions

---
+++ app/controllers/wizards_controller.rb
@@ -0,0 +1,10 @@
+class WizardsController < ApplicationController
+  def part1 # implicitly renders app/views/wizards/part1.html.erb (1st part of signup form)
+  end
+
+  def part2 # implicitly renders app/views/wizards/part2.html.erb (2nd part of signup form)
+  end
+
+  def part3 # implicitly renders app/views/wizards/part3.html.erb (3rd part of signup form)
+  end
+end
