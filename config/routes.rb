4e36d2fcfbec180c9064a71148234904840a5530
github username <github email address>
2018-06-07 12:13:10 -0400

    add wizard resource w/ 3 parts; delete get '/signup', post '/users'

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,26 @@
 Rails.application.routes.draw do
   root 'welcome#home'
 
-  get '/signup' => 'users#new'
-  post '/users' => 'users#create'
+  resource :wizard do
+    get :part1
+    get :part2
+    get :part3
+
+    post :validate_part
+    # Routes generated:
+    # get "/wizard/part1" => wizards#part1
+    # get "/wizard/part2" => wizards#part2
+    # get "/wizard/part3" => wizards#part3
+    # post "/wizard/validate_part" => wizards#validate_part
+  end
+
+  #get '/signup' => 'users#new'
+  #post '/users' => 'users#create'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
 
-  resources :users, only: [:index, :show, :edit, :update, :destroy]
+  #resources :users, only: [:index, :show, :edit, :update, :destroy]
 
   resources :obsessions do
     resources :comments, only: [:create, :index, :edit, :update, :destroy]
