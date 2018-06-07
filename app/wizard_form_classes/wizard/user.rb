9dbb200dde3257359f1eaaede0a4f832163845c8
github username <github email address>
2018-06-07 12:41:08 -0400

    include ActiveModel::Model module in Base class

--- app/wizard_form_classes/wizard/user.rb
+++ app/wizard_form_classes/wizard/user.rb
@@ -1,5 +1,9 @@
 module Wizard
   module User
-    PARTS = %w(part1 part2 part3).freeze
+    PARTS = %w(part1 part2 part3).freeze # PARTS stores the immutable array ["part1", "part2", "part3"]
+
+    class Base
+      include ActiveModel::Model # I can use instances of wizard form classes in Rails forms and access default Rails validations
+    end
   end
 end
