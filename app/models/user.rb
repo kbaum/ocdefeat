4e340d656c418813b7771695c1c1f848ba87ca60
github username <github email address>
2018-06-07 22:49:49 -0400

    remove validation for password presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
   validates :role_requested, inclusion: { in: ["Patient", "Therapist", "Admin"], message: "must be selected from the available roles" }, on: :create
 
   has_secure_password
-  #validates :password, presence: true, length: { minimum: 8 }, allow_nil: true
+  validates :password, length: { minimum: 8 }, allow_nil: true
   # when a user edits their user information, they don't have to retype their password
   def self.find_or_create_by_omniauth(auth_hash)
     self.where(provider: auth_hash["provider"], uid: auth_hash["uid"]).first_or_create do |user|
