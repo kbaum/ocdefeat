2a4138b0c4c9ff73abf9b241f435b6ffc6bdc284
github username <github email address>
2018-06-08 07:25:44 -0400

    generate and run migration to create treatments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180606215636) do
+ActiveRecord::Schema.define(version: 20180608112153) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -70,6 +70,12 @@ ActiveRecord::Schema.define(version: 20180606215636) do
 
   create_table "themes", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "treatments", force: :cascade do |t|
+    t.string "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
