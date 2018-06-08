2e87b64bd4bf844fb8081a41621290e82e4138dd
github username <github email address>
2018-06-08 17:40:34 -0400

    rename type column treatment_type in treatments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180608210445) do
+ActiveRecord::Schema.define(version: 20180608213937) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -75,7 +75,7 @@ ActiveRecord::Schema.define(version: 20180608210445) do
   end
 
   create_table "treatments", force: :cascade do |t|
-    t.string "type"
+    t.string "treatment_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
