7114af2d94c7353ac3d485479396730547fdaeef
github username <github email address>
2018-06-08 12:38:14 -0400

    add skip_before_action :login_required for wizard_parts#index,#show

--- app/controllers/wizard_parts_controller.rb
+++ app/controllers/wizard_parts_controller.rb
@@ -1,4 +1,6 @@
 class WizardPartsController < ApplicationController
+  skip_before_action :login_required, only: [:index, :show]
+
   include Wicked::Wizard
   steps :request_role, :patient_intake, :therapist_credentials
 
