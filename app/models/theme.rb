eaa1edc1935b5421adc6eadfbe50d5c1879113a5
github username <github email address>
2018-06-11 15:19:37 -0400

    delete has_many, has_many through :obsession_themes in Theme model

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,6 +1,4 @@
 class Theme < ApplicationRecord
-  has_many :obsession_themes
-  has_many :obsessions, through: :obsession_themes, dependent: :destroy
   validates :name, presence: true, uniqueness: true
 
   def prevalence_in_patients # instance method returns number of unique users who have at least 1 obsession w/ theme content
