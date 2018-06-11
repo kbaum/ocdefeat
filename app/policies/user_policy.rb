c74b460ec5fd6c8b04ab74b3c935aea28feb9f23
github username <github email address>
2018-06-11 13:36:48 -0400

    only allow therapists to see their own patients in #index?

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -3,9 +3,9 @@ class UserPolicy < ApplicationPolicy
     def resolve
       if user.admin? # admins can see every user on the index page
         scope.all
-      elsif user.therapist? # therapists can only see patients on users index page
-        scope.where(role: 1)
-      elsif user.patient? # patients can only view therapists on users index page
+      elsif user.therapist? # therapists can only see their own patients on users index page
+        scope.where(id: user.counselee_ids)
+      elsif user.patient? # patients can only view therapists on users index page (therapist directory)
         scope.where(role: 2)
       elsif user.unassigned?
         scope.none
