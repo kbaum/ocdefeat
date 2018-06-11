753e543e09388694105bd5294249bc4068a7ab80
github username <github email address>
2018-06-11 15:21:27 -0400

    add has_many :obsessions macro to Theme model

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,4 +1,5 @@
 class Theme < ApplicationRecord
+  has_many :obsessions
   validates :name, presence: true, uniqueness: true
 
   def prevalence_in_patients # instance method returns number of unique users who have at least 1 obsession w/ theme content
