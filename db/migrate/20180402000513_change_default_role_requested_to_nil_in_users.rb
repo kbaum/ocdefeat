e471777dd54c9f7de8e99d1637e08af9c0e8c208
github username <github email address>
2018-06-11 20:06:15 -0400

    delete migrations about severity,role_requested_bio

--- db/migrate/20180402000513_change_default_role_requested_to_nil_in_users.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDefaultRoleRequestedToNilInUsers < ActiveRecord::Migration[5.1]
-  def change
-    change_column_default :users, :role_requested, nil
-  end
-end
--- db/migrate/20180416003044_change_default_severity_to_nil_in_users.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDefaultSeverityToNilInUsers < ActiveRecord::Migration[5.1]
-  def change
-    change_column_default :users, :severity, nil
-  end
-end
