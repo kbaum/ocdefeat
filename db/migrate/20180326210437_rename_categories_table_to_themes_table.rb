b115250197fa02ccca61c540d614d96247c6b246
github username <github email address>
2018-06-11 20:05:03 -0400

    delete migrations for dropping users, adding variant and mentor_id

--- db/migrate/20180326210437_rename_categories_table_to_themes_table.rb
+++
@@ -1,5 +0,0 @@
-class RenameCategoriesTableToThemesTable < ActiveRecord::Migration[5.1]
-  def change
-    rename_table :categories, :themes # rename_table :old_table_name, :new_table_name
-  end
-end
--- db/migrate/20180505014228_add_variant_column_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddVariantColumnToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :variant, :string
-  end
-end
