e905a6cc6052d0382f0bb2e01df61325b3677fa5
github username <github email address>
2018-06-13 09:54:49 -0400

    delete users,obsessions,plans,steps,themes,searches,privacy,terms routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#home'
-
   get '/signup' => 'users#new'
-  post '/users' => 'users#create'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  delete '/logout' => 'sessions#destroy'
-
-  resources :users
-
-  resources :obsessions do
-    resources :comments, only: [:index, :new, :create]
-  end
-
-  resources :plans do
-    resources :steps
-  end
-
-  resources :themes, only: [:index, :new, :create]
-  resources :searches
-
   get '/auth/twitter/callback' => 'sessions#create'
-  get '/privacy' => 'welcome#privacy'
-  get '/terms' => 'welcome#terms'
+  delete '/logout' => 'sessions#destroy'
 end
-
-# resources :users
-
-# resources :obsessions do
-#  resources :comments, only: [:index, :new, :create]
-# end
-
-#  resources :plans do
-#   resources :steps
-#  end
