b7b2115795bf1fb86aef7bd6a3efcad59967f69f
github username <github email address>
2018-06-14 14:43:03 -0400

    store plan instance in else clause of steps#create

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,19 +1,20 @@
 class StepsController < ApplicationController
   before_action :check_completion, only: [:edit, :update]
-  before_action :set_step_and_parent_plan, only: [:show, :destroy]
+  before_action :set_step, only: [:edit, :update, :destroy]
 
   def create # POST request to "/plans/:plan_id/steps" maps to steps#create
-    @step = Step.new(step_params) # new step will automatically belong to plan b/c plan_id is stored in params
+    @step = Step.new(step_params)
     authorize @step
     if @step.save
       redirect_to plan_path(@step.plan), notice: "A new step has been added to this ERP plan!"
     else
+      @plan = @step.plan # to pass to rendered template app/views/plans/show.html.erb
       flash.now[:error] = "Your attempt to add a new step to this ERP plan was unsuccessful. Please try again."
       render "plans/show"
     end
   end
 
-  def edit # GET request to "/plans/:plan_id/steps/:id/edit" maps to steps#edit
+  def edit # GET request to "/steps/:id/edit" maps to steps#edit
     authorize @step
   end
 
@@ -21,9 +22,9 @@ class StepsController < ApplicationController
     authorize @step
     if @step.update(step_params)
       if @step.complete? # If the step is updated from incomplete to complete (status changes from 0 to 1)
-        redirect_to plan_path(@plan), notice: "Milestone accomplished! You're one step closer to defeating OCD!"
+        redirect_to plan_path(@step.plan), notice: "Milestone accomplished! You're one step closer to defeating OCD!"
       else
-        redirect_to plan_path(@plan), notice: "You successfully modified a step in this ERP plan!"
+        redirect_to plan_path(@step.plan), notice: "You successfully modified a step in this ERP plan!"
       end
     else
       flash.now[:error] = "Your attempt to edit this ERP exercise was unsuccessful. Please try again."
@@ -31,26 +32,24 @@ class StepsController < ApplicationController
     end
   end
 
-  def destroy # deleting a step - DELETE request to "/plans/:plan_id/steps/:id" mapped to steps#destroy
+  def destroy # deleting a step - DELETE request to "/plans/:plan_id/steps/:id" maps to steps#destroy
     authorize @step
     @step.destroy
-    redirect_to plan_path(@step.plan), notice: "A step was successfully deleted from this ERP plan!"
+    redirect_to plan_path(@plan), notice: "A step was successfully deleted from this ERP plan!"
   end
 
   private
 
     def check_completion
-      @plan = Plan.find(params[:plan_id])
-      @step = @plan.steps.find(params[:id])
+      @step = Step.find(params[:id])
       if @step.complete?
-        redirect_to plan_path(@plan), alert: "You already performed this ERP exercise, so there is no need to modify this step!"
+        redirect_to plan_path(@step.plan), alert: "You already performed this ERP exercise, so there is no need to modify this step!"
       end
     end
 
-    # Finding an associated step - only finding step that already belongs to that plan - 2 queries but protecting against URL hack
-    def set_step_and_parent_plan # called before steps#edit, steps#update, steps#destroy
-      @plan = Plan.find(params[:plan_id])
-      @step = @plan.steps.find(params[:id])
+    def set_step
+      @step = Step.find(params[:id])
+      @plan = @step.plan
     end
 
     def step_params
