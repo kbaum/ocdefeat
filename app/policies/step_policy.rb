22bf57cf3735717855b088c39b1b697e8229a522
github username <github email address>
2018-06-14 15:06:11 -0400

    define #permitted_attributes in StepPolicy to prevent updating :plan_id

--- app/policies/step_policy.rb
+++ app/policies/step_policy.rb
@@ -3,6 +3,12 @@ class StepPolicy < ApplicationPolicy # A step does not exist outside the context
     user.patient? || user.therapist?
   end
 
+  def permitted_attributes # once the step's :plan_id was assigned in steps#create, it cannot be changed
+    if user.therapist? || plan_possessor
+      [:instructions, :duration, :discomfort_degree, :status]
+    end
+  end
+
   def update? # Therapists and the patient who created the plan to which the step belongs can edit the step
     plan_possessor || user.therapist?
   end
