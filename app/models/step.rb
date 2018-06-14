02401386f9bf81af5cf4d7185de42a690c4bcaee
github username <github email address>
2018-06-14 17:12:18 -0400

    add validates :status, status: true to Step model

--- app/models/step.rb
+++ app/models/step.rb
@@ -3,14 +3,15 @@ class Step < ApplicationRecord
     :incomplete => 0,
     :complete => 1
   }
-  
+
   scope :not_performed, -> { where(status: 0) }
 
-  belongs_to :plan # therefore, steps table has plan_id foreign key column
+  belongs_to :plan
 
   validates :instructions, presence: true
-  validates :duration, presence: true # duration is a string, such as "1 hour", "15 minutes"
+  validates :duration, presence: true
   validates :discomfort_degree, inclusion: { in: 1..10 }, on: :update, allow_blank: true
+  validates :status, status: true
 
   def complete? # a completed step (self) has a status attribute value = 1
     self.status == STEP_STATUS[:complete]
