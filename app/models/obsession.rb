47befde36cdae24f5ebd2aceda177f0362155a19
github username <github email address>
2018-06-19 17:46:19 -0400

    define scope :sans_plans using where.not in Obsession model

--- app/models/obsession.rb
+++ app/models/obsession.rb
@@ -1,7 +1,7 @@
 class Obsession < ApplicationRecord
   scope :defeatable_by_flooding, -> { joins(:plans).merge(Plan.flooding).distinct }
   scope :defeatable_by_graded_exposure, -> { joins(:plans).merge(Plan.graded_exposure).distinct }
-  
+  scope :sans_plans, -> { where.not(id: Plan.all.map {|plan| plan.obsession_id}) }
   scope :presenting_symptoms, -> { where.not(symptoms: ["", " "]) }
   scope :symptomless, -> { where(symptoms: ["", " "]) }
 
