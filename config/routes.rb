9dabaf991cde070e44ebd4e275f1a6fa628e392f
github username <github email address>
2018-06-22 15:22:16 -0400

    add get '/about' => 'welcome#about' route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#home'
+  get '/about' => 'welcome#about'
   get '/signup' => 'users#new'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
