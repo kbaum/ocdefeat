f8b98a901f5b01ba97b4a7ed365e45735dfdec55
github username <github email address>
2018-06-22 07:17:58 -0400

    add uniqueness validation of treatment_type in Treatment model

--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -2,5 +2,5 @@ class Treatment < ApplicationRecord
   has_many :user_treatments
   has_many :users, through: :user_treatments
 
-  validates :treatment_type, presence: true
+  validates :treatment_type, presence: true, uniqueness: true
 end
