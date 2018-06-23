a392684e4a5325a8fd776565a2421b14c5bf836e
github username <github email address>
2018-06-23 14:19:30 -0400

    add #edit? to CommentPolicy class

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -3,6 +3,10 @@ class CommentPolicy < ApplicationPolicy
     user.patient? || user.therapist?
   end
 
+  def edit?
+    comment_owner
+  end
+
   def update?
     comment_owner
   end
@@ -17,3 +21,6 @@ class CommentPolicy < ApplicationPolicy
       record.user == user
     end
 end
+
+# A patient can only comment on her own obsessions (a patient can only see her own obsessions' show pages)
+# A therapist can only comment on her own patients' obsessions (a therapist can only see her own patients' obsessions' show pages)
