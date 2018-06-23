8d7efc74341cb60e8d341ad03c275685c5dce433
github username <github email address>
2018-06-23 14:32:51 -0400

    change user_id to user in scope :advice, :concerns

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,11 @@
 class Comment < ApplicationRecord
-  scope :advice, -> { where(user_id: User.therapists) }
-  scope :concerns, -> { where(user_id: User.patients) }
+  scope :advice, -> { where(user: User.therapists) }
+  scope :concerns, -> { where(user: User.patients) }
 
   belongs_to :obsession
   belongs_to :user
+
   delegate :name, to: :user, prefix: true, allow_nil: true # calling #user_name on comment instance returns the name attribute value of the user instance to which the comment belongs.
+
   validates :content, presence: true
 end
