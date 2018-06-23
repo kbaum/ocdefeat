d0074e79a7b77777dafecbada0df37c65032481f
github username <github email address>
2018-06-23 09:38:47 -0400

    add dependent: :destroy; delete presence validation

--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -1,6 +1,5 @@
 class Treatment < ApplicationRecord
-  has_many :user_treatments
+  has_many :user_treatments, dependent: :destroy
   has_many :users, through: :user_treatments
-
-  validates :treatment_type, presence: true, uniqueness: true
+  validates :treatment_type, uniqueness: true
 end
