7bd28f2ea2635e6ed2252a8f45fbb8e3d5eb22b6
github username <github email address>
2018-06-24 08:22:33 -0400

    call #update_attributes(permitted_attributes(@comment)) in #update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,7 +18,7 @@ class CommentsController < ApplicationController
 
   def update # PUT or PATCH request to "/comments/:id" maps to comments#update due to shallow nesting
     authorize @comment
-    if @comment.update(comment_params)
+    if @comment.update_attributes(permitted_attributes(@comment))
       redirect_to obsession_comments_path(@comment.obsession), notice: "Your comment was successfully modified!"
     else
       flash.now[:error] = "Your attempt to edit this comment was unsuccessful. Please try again."
