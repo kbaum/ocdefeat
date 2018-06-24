99be3544830512ff5caaa60bd87586738d49be57
github username <github email address>
2018-06-24 13:37:19 -0400

    add description column to themes table with string datatype values

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180621152827) do
+ActiveRecord::Schema.define(version: 20180624173633) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20180621152827) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
   create_table "treatments", force: :cascade do |t|
