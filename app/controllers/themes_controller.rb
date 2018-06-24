5657762f340514b796e70407737ade769e4e20af
github username <github email address>
2018-06-24 15:34:30 -0400

    define themes#create

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -5,6 +5,18 @@ class ThemesController < ApplicationController
     @theme = Theme.new # instance for form_for to wrap around
   end
 
+  def create
+    @theme = Theme.new(theme_params)
+    authorize @theme
+
+    if @theme.save
+      flash.now[:notice] = "You created the OCD theme \"#{Theme.last.name}\" in which to classify your patients' obsessions!"
+    else
+      flash.now[:alert] = "Notwithstanding your psychological expertise, your attempt to create a unique OCD theme was unsuccessful. Please try again."
+      render :new
+    end
+  end
+
   def index
     themes = policy_scope(Theme)
     @new_theme = Theme.new # instance for form_for to wrap around when therapist creates new theme on themes index page
