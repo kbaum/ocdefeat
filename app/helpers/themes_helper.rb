ead3cb71d12703b6f99b14193eb90fb8cb4bd4ca
github username <github email address>
2018-06-24 16:45:15 -0400

    delete elsif logic in #tally_popularity(theme)

--- app/helpers/themes_helper.rb
+++ app/helpers/themes_helper.rb
@@ -1,18 +1,8 @@
 module ThemesHelper
-  def describe_theme(theme)
-    theme.description.empty? ? "Self-explanatory" : "#{theme.description}"
-  end
-
   def tally_popularity(theme)
     if theme.obsessions_per_theme == 0
       "No patients are currently obsessing about \"#{theme.name}.\""
     else
-      <<-HEREDOC
-      #{theme.prevalence_in_patients} #{'patient'.pluralize(theme.prevalence_in_patients)}
-      #{'is'.pluralize(theme.prevalence_in_patients)} obsessing about #{theme.name}, in which
-      #{theme.obsessions_per_theme} #{'obsession'.pluralize(theme.obsessions_per_theme)}
-      #{'is'.pluralize(theme.obsessions_per_theme)} classified.
-      HEREDOC
     end
   end
 end
