ea39b80bef0940f6359e940b676067de286a3f97
github username <github email address>
2018-06-24 16:15:43 -0400

    delete 4 class methods in Theme model

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -6,23 +6,7 @@ class Theme < ApplicationRecord
     User.obsessing_about(self).count
   end
 
-  def self.least_to_most_prevalent # class method returns array of theme instances ordered by those w/ the least to greatest number of unique users having obsessions about these themes
-    self.all.sort_by {|theme| theme.prevalence_in_patients}
-  end
-
-  def self.most_to_least_prevalent # class method returns array of theme instances ordered by those w/ the greatest to least number of unique users having obsessions about these themes
-    self.all.sort {|a,b| b.prevalence_in_patients <=> a.prevalence_in_patients}
-  end
-
   def obsessions_per_theme
     obsessions.count
-  end
-
-  def self.least_to_most_obsessions # class method returns array of theme instances ordered by those with the least to most obsessions classified in them
-    self.all.sort_by {|theme| theme.obsessions_per_theme}
-  end
-
-  def self.most_to_least_obsessions # class method returns array of theme instances ordered by those with the most to least obsessions classified in them
-    self.all.sort {|a,b| b.obsessions_per_theme <=> a.obsessions_per_theme}
   end
 end
