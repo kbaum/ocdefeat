8343304028369014978235287b70041843d879ab
github username <github email address>
2018-06-24 16:11:50 -0400

    call #obsessing_about(theme)in prevalence_in_patients; remove self from #obsessions_per_theme

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -3,7 +3,7 @@ class Theme < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   def prevalence_in_patients # instance method returns number of unique users who have at least 1 obsession w/ theme content
-    self.obsessions.map {|o| o.user}.uniq.count
+    User.obsessing_about(self).count
   end
 
   def self.least_to_most_prevalent # class method returns array of theme instances ordered by those w/ the least to greatest number of unique users having obsessions about these themes
@@ -14,8 +14,8 @@ class Theme < ApplicationRecord
     self.all.sort {|a,b| b.prevalence_in_patients <=> a.prevalence_in_patients}
   end
 
-  def obsessions_per_theme # instance method called on theme instance returns the number of obsessions classified in theme
-    self.obsessions.count
+  def obsessions_per_theme
+    obsessions.count
   end
 
   def self.least_to_most_obsessions # class method returns array of theme instances ordered by those with the least to most obsessions classified in them
