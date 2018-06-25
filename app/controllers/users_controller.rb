87191c61e31d568416e8d99302d4bf8d587b73bb
github username <github email address>
2018-06-25 15:43:13 -0400

    edit flash alert msgs about severity and variant

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,19 +33,19 @@ class UsersController < ApplicationController
       else
         @filtered_users = users # Admin did not choose a filter, so @filtered_users stores all users
       end
-    elsif current_user.therapist? # When therapist views users index page, users variable stores all patients
+    elsif current_user.therapist? # When therapist views users index page, users variable stores the therapist's patients
       if !params[:severity].blank? # Therapist filters patients by OCD severity
         if users.by_ocd_severity(params[:severity]).empty? # If no patients have the selected OCD severity
-          flash.now[:alert] = "No patients were diagnosed with #{params[:severity]} OCD."
+          flash.now[:alert] = "None of your patients were diagnosed with #{params[:severity]} OCD."
         else
           @filtered_users = users.by_ocd_severity(params[:severity]) # stores AR::Relation of all patients with the selected OCD severity
           flash.now[:notice] = "#{sv_agreement(@filtered_users)} diagnosed with #{params[:severity]} OCD!"
         end
       elsif !params[:variant].blank? # Therapist filters patients by OCD variant
-        if users.by_ocd_variant(params[:variant]).empty? # If no patients have the specified OCD variant
-          flash.now[:alert] = "No patients with that variant of OCD were found."
+        if users.by_ocd_variant(params[:variant]).empty? # If none of the therapist's patients have the specified OCD variant
+          flash.now[:alert] = "None of your patients were diagnosed with that variant of OCD."
         else
-          @filtered_users = users.by_ocd_variant(params[:variant]) # stores AR::Relation of all patients with a specific OCD variant
+          @filtered_users = users.by_ocd_variant(params[:variant]) # stores AR::Relation of the therapist's patients with a specific OCD variant
           if params[:variant] == "Both"
             flash.now[:notice] = "#{sv_agreement(@filtered_users)} both traditionally and purely obsessive."
           elsif params[:variant] == "Purely Obsessional"
