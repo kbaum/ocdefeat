d27f041698947bf1351ba23df411932bfae50d3b
github username <github email address>
2018-06-27 14:43:33 -0400

    call #without_step_or_with_incomplete_step?, not #update

--- app/validators/progress_validator.rb
+++ app/validators/progress_validator.rb
@@ -1,6 +1,6 @@
 class ProgressValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    if value == 1 && record.unachieved?
+    if value == 1 && record.without_step_or_with_incomplete_step?
       record.errors[attribute] << (options[:message] || "toward achieving your goals depends on the creation and completion of exposure exercises!")
     end
   end
