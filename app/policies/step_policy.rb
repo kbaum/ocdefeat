697f217d6fa0ec62f2dd0569e597b789425a288d
github username <github email address>
2018-06-27 13:30:49 -0400

    edit comments in StepPolicy

--- app/policies/step_policy.rb
+++ app/policies/step_policy.rb
@@ -1,18 +1,17 @@
 class StepPolicy < ApplicationPolicy # A step does not exist outside the context of an ERP plan
-
-  def create? # The patient who owns the plan off of which the step will be built, and the patient's therapist, can create a new step for that plan
+  def create?
     user.patient? || user.therapist?
   end
 
   def edit? # Note: if you can't view the parent plan, you can't edit the step
     parent_plan_possessor || therapist_of_parent_plan_possessor
   end
-# The patient who created the plan to which the step belongs and that patient's therapist can edit the step
+
   def update?
     parent_plan_possessor || therapist_of_parent_plan_possessor
   end
 
-  def destroy? # The patient who created the plan to which the step belongs and that patient's therapist can delete the step
+  def destroy?
     parent_plan_possessor || therapist_of_parent_plan_possessor
   end
 
@@ -28,3 +27,5 @@ class StepPolicy < ApplicationPolicy # A step does not exist outside the context
       end
     end
 end
+# The patient who created the plan to which the step belongs and that patient's therapist
+# can edit the step, delete the step or add a new step to the plan
