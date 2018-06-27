a73f8a3ab894d799e0bacb7f3b92b464c2eb3945
github username <github email address>
2018-06-27 07:55:32 -0400

    delete permitted_attributes;call private methods in edit?

--- app/policies/step_policy.rb
+++ app/policies/step_policy.rb
@@ -1,17 +1,14 @@
 class StepPolicy < ApplicationPolicy # A step does not exist outside the context of an ERP plan
-  def create? # The patient who owns the plan to which the step belongs, and the patient's therapist, can create a new step for that plan
+
+  def create? # The patient who owns the plan off of which the step will be built, and the patient's therapist, can create a new step for that plan
     user.patient? || user.therapist?
   end
 
-  def permitted_attributes # once the step's :plan_id was assigned in steps#create, it cannot be changed
-    if parent_plan_possessor # The patient who owns the plan to which the step belongs
-      [:instructions, :duration, :discomfort_degree, :status]
-    elsif therapist_of_parent_plan_possessor # That patient's therapist
-      [:instructions, :duration]
-    end
+  def edit? # Note: if you can't view the parent plan, you can't edit the step
+    parent_plan_possessor || therapist_of_parent_plan_possessor
   end
-
-  def update? # The patient who created the plan to which the step belongs and that patient's therapist can edit the step
+# The patient who created the plan to which the step belongs and that patient's therapist can edit the step
+  def update?
     parent_plan_possessor || therapist_of_parent_plan_possessor
   end
 
