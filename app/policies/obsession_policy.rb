e35dda7ea411f7a97fbf9ef61c08392788ed2073
github username <github email address>
2018-06-28 18:50:13 -0400

    call #obsession_owner in #update? in ObsessionPolicy

--- app/policies/obsession_policy.rb
+++ app/policies/obsession_policy.rb
@@ -1,9 +1,9 @@
 class ObsessionPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      if user.admin? # An admin can view the index of all obsessions (thought is displayed anonymously)
+      if user.admin? # An admin can view the index of all obsessions (intrusive_thought is displayed anonymously)
         scope.all
-      elsif user.therapist? # A therapist can view the index of her own counselees' obsessions
+      elsif user.therapist? # A therapist can view the index of her own patients' obsessions
         scope.where(user: user.counselees)
       elsif user.patient? # For confidentiality purposes, a patient can only view her own obsessions
         scope.where(user: user)
@@ -22,7 +22,7 @@ class ObsessionPolicy < ApplicationPolicy
   def show?
     if user.patient? # A patient can only see her own obsessions' show pages
       obsession_owner
-    elsif user.therapist? # A therapist can only see her own counselees' obsession show pages
+    elsif user.therapist? # A therapist can only see her own patients' obsession show pages
       record.user.in?(user.counselees)
     end
   end
@@ -31,14 +31,14 @@ class ObsessionPolicy < ApplicationPolicy
     obsession_owner
   end
 
-  def permitted_attributes # user_id cannot be changed
+  def permitted_attributes # user_id of obsession instance cannot be changed
     if obsession_owner
       [:intrusive_thought, :triggers, :anxiety_rating, :symptoms, :rituals, :time_consumed, :theme_id]
     end
   end
 
   def update?
-    edit?
+    obsession_owner
   end
 
   def destroy?
