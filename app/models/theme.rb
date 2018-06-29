008da71f0b40c0afda9bda4cd71273a398db2b8e
github username <github email address>
2018-06-29 11:51:45 -0400

    add delegate :name, to: :user, prefix 'contributor' to Theme model

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -3,6 +3,8 @@ class Theme < ApplicationRecord
   has_many :obsessions
   validates :name, presence: true, uniqueness: true
 
+  delegate :name, to: :user, prefix: "contributor" # I can call #contributor_name on theme instance to return the name of therapist user who created the theme
+
   def prevalence_in_patients # instance method returns number of unique users who have at least 1 obsession w/ theme content
     User.obsessing_about(self).count
   end
