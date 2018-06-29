03c985065455d20863cc2307e9a01856fdc270e9
github username <github email address>
2018-06-29 10:27:46 -0400

    add except: :about to before_action :login_required

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-  before_action :login_required, except: [:home]
+  before_action :login_required, except: [:home, :about]
 
   private
 
