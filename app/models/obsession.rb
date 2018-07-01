004765a2a520fe838ac3c7c213aee8c11cbe3f5e
github username <github email address>
2018-07-01 18:42:05 -0400

    change array of blank strings to nil in scope :presenting_symptoms

--- app/models/obsession.rb
+++ app/models/obsession.rb
@@ -5,7 +5,7 @@ class Obsession < ApplicationRecord
   scope :defeatable_by_flooding, -> { joins(:plans).merge(Plan.flooding).distinct }
   scope :defeatable_by_graded_exposure, -> { joins(:plans).merge(Plan.graded_exposure).distinct }
   scope :sans_plans, -> { where.not(id: Plan.all.map {|plan| plan.obsession_id}) }
-  scope :presenting_symptoms, -> { where.not(symptoms: ["", " "]) }
+  scope :presenting_symptoms, -> { where.not(symptoms: nil) }
   scope :symptomless, -> { where(symptoms: ["", " "]) }
 
   belongs_to :theme
