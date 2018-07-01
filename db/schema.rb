40c3d7280df3a8740240b96c5fe46154c43cba48
github username <github email address>
2018-07-01 17:34:28 -0400

    change default completed column value to false in steps table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180701212357) do
+ActiveRecord::Schema.define(version: 20180701213201) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 20180701212357) do
     t.integer "plan_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "completed"
+    t.boolean "completed", default: false
   end
 
   create_table "themes", force: :cascade do |t|
