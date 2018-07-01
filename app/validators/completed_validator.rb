5972f2fa6001318b7f0524dd35380a103297146c
github username <github email address>
2018-07-01 17:26:36 -0400

    rename CompletedValidator; change complete to completed

--- app/validators/completed_validator.rb
+++ app/validators/completed_validator.rb
@@ -1,4 +1,4 @@
-class CompleteValidator < ActiveModel::EachValidator
+class CompletedValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     if value == true && !record.discomfort_degree.present?
       record.errors[attribute] << (options[:message] || "is a valid status only if you already rated your degree of discomfort when performing this step!")
