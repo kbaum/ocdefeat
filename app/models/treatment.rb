00240832318be3ee741b35b91e3766dc6b7672f6
github username <github email address>
2018-07-02 14:17:07 -0400

    replace patient_treatments w/ user_treatments, patients w/ users

--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -1,7 +1,5 @@
 class Treatment < ApplicationRecord
-  #has_many :user_treatments, dependent: :destroy
-  #has_many :users, through: :user_treatments
-  has_many :patient_treatments, dependent: :destroy
-  has_many :patients, through: :patient_treatments
+  has_many :user_treatments, dependent: :destroy
+  has_many :users, through: :user_treatments
   validates :treatment_type, uniqueness: true
 end
