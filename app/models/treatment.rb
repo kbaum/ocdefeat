8c3625c86122c11d1cfe1fd971b5a034e071bdf6
github username <github email address>
2018-07-01 22:01:01 -0400

    add has_many, has_many :through macros to Treatment model

--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -1,5 +1,7 @@
 class Treatment < ApplicationRecord
-  has_many :user_treatments, dependent: :destroy
-  has_many :users, through: :user_treatments
+  #has_many :user_treatments, dependent: :destroy
+  #has_many :users, through: :user_treatments
+  has_many :patient_treatments, dependent: :destroy
+  has_many :patients, through: :patient_treatments
   validates :treatment_type, uniqueness: true
 end
