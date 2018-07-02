ab8aa56f3f5171af59ae7c71ef7d8bf31af1a705
github username <github email address>
2018-07-01 21:45:49 -0400

    create admins table and Admin model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180702014114) do
+ActiveRecord::Schema.define(version: 20180702014459) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
