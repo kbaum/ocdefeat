e40a4905da41c5338b82e623d93789026a71e7bf
github username <github email address>
2018-07-01 21:36:00 -0400

    run migration to create patients table and Patient model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180701213201) do
+ActiveRecord::Schema.define(version: 20180702012912) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -31,6 +31,19 @@ ActiveRecord::Schema.define(version: 20180701213201) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "theme_id"
+  end
+
+  create_table "patients", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.string "uid"
+    t.string "provider"
+    t.string "severity"
+    t.string "variant"
+    t.integer "therapist_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "plans", force: :cascade do |t|
