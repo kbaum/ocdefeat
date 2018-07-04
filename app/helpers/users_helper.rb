59aa02bf082b773e2c4d9b872435c1214c916ddc
github username <github email address>
2018-07-04 17:10:59 -0400

    define #symptoms_in(users) helper in UsersHelper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,14 @@
 module UsersHelper
+  def symptoms_in(users) # argument is AR::Relation of a therapist's patients
+    if users.all? {|user| user.obsessions.empty?} # If none of the therapist's patients have obsessions
+      content_tag(:small, "All of your patients are asymptomatic since none of them are obsessing!")
+    elsif users.patients_nonobsessive.empty? && users.patients_obsessive_but_symptomless.empty? # All of the therapist's patients have obsessions, and all of these obsessions have symptoms
+      content_tag(:small, "All of your OCD patients are physically symptomatic.")
+    elsif users.patients_nonobsessive.empty? && users.symptomatic.empty? # # All of the therapist's patients have obsessions, but none of these obsessions have symptoms
+      content_tag(:small, "None of your OCD patients is physically symptomatic.")
+    end
+  end
+
   def demand_data(attribute_name) # argument is the string "severity" or "variant"
     if current_user.patient?
       content_tag(:div, class: "alert alert-warning", role: "alert") do
