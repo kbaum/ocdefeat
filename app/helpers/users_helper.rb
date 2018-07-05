14a20ad10dff23ba09d25d7bd16a8aeb34b6ec21
github username <github email address>
2018-07-05 16:11:02 -0400

    change text 'since none obsesses' to 'and nonobsessive'

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -9,7 +9,7 @@ module UsersHelper
 
   def symptoms_in(users) # argument is AR::Relation of a therapist's patients
     if users.all? {|user| user.obsessions.empty?} # If none of the therapist's patients have obsessions
-      content_tag(:small, "All of your patients are asymptomatic since none obsesses!")
+      content_tag(:small, "All of your patients are asymptomatic and nonobsessive!")
     elsif users.patients_nonobsessive.empty? && users.patients_obsessive_but_symptomless.empty? # All of the therapist's patients have obsessions, and all of these obsessions have symptoms
       content_tag(:small, "All of your OCD patients are physically symptomatic.")
     elsif users.patients_nonobsessive.empty? && users.symptomatic.empty? # # All of the therapist's patients have obsessions, but none of these obsessions have symptoms
