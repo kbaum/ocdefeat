7ad1bb664aa20189b2e18bee6e2ecd29283fbfa9
github username <github email address>
2018-07-05 19:54:16 -0400

    add attr_accessor :default_scope, #initialize in PatientObsessionFinder

--- app/queries/patient_obsession_finder.rb
+++ app/queries/patient_obsession_finder.rb
@@ -1,2 +1,7 @@
 class PatientObsessionFinder
+  attr_accessor :default_scope
+
+  def initialize(default_scope) # default scope is AR::Relation of the patient's own obsessions
+    @default_scope = default_scope
+  end
 end
