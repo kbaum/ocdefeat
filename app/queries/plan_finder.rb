fbf0e1aa6c1d0b8cba4241cb98711dd778197a8f
github username <github email address>
2018-07-07 20:22:05 -0400

    define #filter_by_obsession_targeted(scoped, obsession_targeted) in PlanFinder

--- app/queries/plan_finder.rb
+++ app/queries/plan_finder.rb
@@ -4,4 +4,8 @@ class PlanFinder
   def initialize(default_scope) # the patient's own plans or the therapist's patients' plans
     @default_scope = default_scope
   end
+
+  def filter_by_obsession_targeted(scoped, obsession_targeted)
+    obsession_targeted.blank? ? scoped : scoped.where(obsession: obsession_targeted)
+  end
 end
