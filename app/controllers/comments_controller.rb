11a689fdea0809a811a0fd0aaa30a915407ad059
github username <github email address>
2018-07-09 17:53:56 -0400

    delete if notparams[:type];change 'coping with' to 'overcoming'

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -38,21 +38,19 @@ class CommentsController < ApplicationController
     patient_obsessing = @obsession.user # stores the patient who reported the obsession
     authorize patient_obsessing, :show_comments? # A patient can see all comments on her own obsessions. A therapist can see all comments on her own patients' obsessions.
 
-    if !params[:type].blank? # If the user filters comments by type
-      if params[:type] == "Patient Concerns"
-        if @comments.concerns.empty?
-          flash.now[:alert] = "#{commenter} voiced no further concerns about this obsession, but here are some therapy tips to keep in mind:"
-        else
-          @comments = @comments.concerns # stores all concerns on a single obsession
-          flash.now[:notice] = "#{commenter} voiced #{@comments.count} #{'concern'.pluralize(@comments.count)} about this obsession."
-        end
-      elsif params[:type] == "Advice from Therapists"
-        if @comments.advice.empty?
-          flash.now[:alert] = "Unfortunately, no therapy pointers were given to #{commenter.downcase}, but here are some concerns that need to be addressed:"
-        else
-          @comments = @comments.advice # stores all advice on a single obsession
-          flash.now[:notice]= "#{commenter} should bear #{@comments.count} therapy #{'pointer'.pluralize(@comments.count)} in mind when coping with this obsession."
-        end
+    if params[:type] == "Patient Concerns"
+      if @comments.concerns.empty?
+        flash.now[:alert] = "#{commenter} voiced no further concerns about this obsession, but here are some therapy tips to keep in mind:"
+      else
+        @comments = @comments.concerns # stores all concerns on a single obsession
+        flash.now[:notice] = "#{commenter} voiced #{@comments.count} #{'concern'.pluralize(@comments.count)} about this obsession."
+      end
+    elsif params[:type] == "Advice from Therapists"
+      if @comments.advice.empty?
+        flash.now[:alert] = "Unfortunately, no therapy pointers were given to #{commenter.downcase}, but here are some concerns that need to be addressed:"
+      else
+        @comments = @comments.advice # stores all advice on a single obsession
+        flash.now[:notice]= "#{commenter} should bear #{@comments.count} therapy #{'pointer'.pluralize(@comments.count)} in mind when overcoming this obsession."
       end
     end
   end
