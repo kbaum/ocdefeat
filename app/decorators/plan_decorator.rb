5a7388cbbf241c80fec31ff3ae0b7101905cd231
github username <github email address>
2018-07-09 19:47:04 -0400

    define PlanDecorator w/ #present_progress

---
+++ app/decorators/plan_decorator.rb
@@ -0,0 +1,17 @@
+class PlanDecorator < SimpleDelegator
+  def present_progress
+    if finished?
+      "Accomplished! (Fully implemented and marked as finished)"
+    else
+      if steps.empty?
+        "Preliminary Design (Lacks steps)"
+      else
+        if steps.all? {|step| step.completed?}
+          "Development or Pending Submission (Fully performed but not marked finished)"
+        else
+          "Development or Implementation (Contains at least one incomplete step)"
+        end
+      end
+    end
+  end
+end
