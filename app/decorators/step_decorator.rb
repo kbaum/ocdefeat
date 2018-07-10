a0cf2d0cfbb6b42b8330bafa0c4e1a7ad94916b7
github username <github email address>
2018-07-10 17:45:22 -0400

    delete present_position_in_plan,display_discomfort(user)

--- app/decorators/step_decorator.rb
+++ app/decorators/step_decorator.rb
@@ -1,23 +1,7 @@
 class StepDecorator < Draper::Decorator
   delegate_all
-  
+
   def last_updated
     updated_at.strftime("Last modified on %A, %B %e, at %l:%M %p")
-  end
-
-  def display_discomfort(user)
-    if discomfort_degree.nil?
-      if user.patient?
-        link_to("Rate your discomfort", edit_step_path(__getobj__)) << " when performing this step."
-      elsif user.therapist? && plan.user.in?(user.counselees)
-        "Not yet rated"
-      end
-    else
-      discomfort_degree
-    end
-  end
-
-  def present_position_in_plan
-    plan.steps.find_index(__getobj__).to_i + 1
   end
 end
