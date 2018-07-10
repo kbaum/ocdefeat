7abf7ee48af424bf1c9324358096bf1a3e9721e0
github username <github email address>
2018-07-09 21:47:27 -0400

    include ActionView::Helpers,Rails.application.routes.url_helpers

--- app/decorators/step_decorator.rb
+++ app/decorators/step_decorator.rb
@@ -1,4 +1,7 @@
 class StepDecorator < SimpleDelegator
+  include ActionView::Helpers
+  include Rails.application.routes.url_helpers
+
   def last_updated
     updated_at.strftime("Last modified on %A, %B %e, at %l:%M %p")
   end
