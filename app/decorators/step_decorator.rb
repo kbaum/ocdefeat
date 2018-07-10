78a2f6e029909fe0ee5c2b913e2f765206174f45
github username <github email address>
2018-07-10 17:11:31 -0400

    inherit from Draper::Decorator;delete 2 include statements, call delegate_all

--- app/decorators/step_decorator.rb
+++ app/decorators/step_decorator.rb
@@ -1,7 +1,6 @@
-class StepDecorator < SimpleDelegator
-  include ActionView::Helpers
-  include Rails.application.routes.url_helpers
-
+class StepDecorator < Draper::Decorator
+  delegate_all
+  
   def last_updated
     updated_at.strftime("Last modified on %A, %B %e, at %l:%M %p")
   end
