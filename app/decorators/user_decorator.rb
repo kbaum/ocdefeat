93432e67ef5c5c61491fddffc4ad0b517c8438cc
github username <github email address>
2018-07-11 08:25:30 -0400

    define #pronouns_for_conquering_ocd, #pronouns_for_planning

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -4,4 +4,12 @@ class UserDecorator < ApplicationDecorator
   def objects_count(string_object) # argument is a string like "obsession" or "plan"
     helpers.pluralize user.send("#{string_object}s").count, "#{string_object}"
   end
+
+  def pronouns_for_conquering_ocd
+    current_user.patient? ? "You are currently conquering" : "The patient is currently conquering"
+  end
+
+  def pronouns_for_planning
+    current_user.patient? ? "You designed" : "The patient designed"
+  end
 end
