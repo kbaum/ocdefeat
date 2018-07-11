c3dcbee3f6fea61064b18f93119ffbdabd218c2a
github username <github email address>
2018-07-11 19:55:49 -0400

    delete label_submit_btn from CommentsHelper

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,12 +1,4 @@
 module CommentsHelper
-  def label_submit_btn
-    if current_user.therapist?
-      "Share Your Advice"
-    elsif current_user.patient?
-      "Share Your Troubles"
-    end
-  end
-
   def reassurance_gerunds
     if current_user.patient?
       "seeking"
