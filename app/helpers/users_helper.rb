830c06c10d81e827c4cdb72b4946844813a00e27
github username <github email address>
2018-07-11 17:58:31 -0400

    define #summarize_symptoms in UsersHelper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,4 +8,18 @@ module UsersHelper
       end
     end
   end
+
+  def summarize_symptoms
+    if current_user.therapist?
+      symptoms_summary =
+        if current_user.counselees.all? {|counselee| counselee.obsessions.empty?} # If none of the therapist's patients have obsessions
+          "All of your patients are asymptomatic and nonobsessive!"
+        elsif current_user.counselees.patients_nonobsessive.empty? && current_user.counselees.patients_obsessive_but_symptomless.empty? # All of the therapist's patients have obsessions, and all of these obsessions have symptoms
+          "All of your OCD patients are physically symptomatic."
+        elsif current_user.counselees.patients_nonobsessive.empty? && current_user.counselees.symptomatic.empty? # All of the therapist's patients have obsessions, but none of these obsessions have symptoms
+          "None of your OCD patients is physically symptomatic."
+        end
+      content_tag(:small, symptoms_summary)
+    end
+  end
 end
