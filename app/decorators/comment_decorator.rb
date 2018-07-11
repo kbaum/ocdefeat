d1bbc269861325c371b6c0aca57b5774248e8433
github username <github email address>
2018-07-11 19:44:09 -0400

    define #placefill in CommentDecorator

--- app/decorators/comment_decorator.rb
+++ app/decorators/comment_decorator.rb
@@ -9,4 +9,8 @@ class CommentDecorator < ApplicationDecorator
       "Voice your concerns to a counselor about this obsession"
     end
   end
+
+  def placefill
+    current_user.patient? ? "Feel free to vent here..." : "Add mental health tips..."
+  end
 end
