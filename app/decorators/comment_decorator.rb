8e1c461b179dddc98b3530c5a32ed4c403206483
github username <github email address>
2018-07-11 19:31:10 -0400

    define #label in CommentDecorator

--- app/decorators/comment_decorator.rb
+++ app/decorators/comment_decorator.rb
@@ -1,4 +1,12 @@
 class CommentDecorator < ApplicationDecorator
   include Draper::LazyHelpers
   delegate_all
+
+  def label
+    if current_user.therapist?
+      "Give the patient some therapy pointers to defeat this obsession:"
+    elsif current_user.patient?
+      "Voice your concerns to a counselor about this obsession:"
+    end
+  end
 end
