3e36af3e3d7e26af8f649948a39a25e2c215dc61
github username <github email address>
2018-07-11 17:55:30 -0400

    delete show_symptoms, define #present_patients(patients)

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,11 @@
 module UsersHelper
-  def symptoms_in(users) # argument is AR::Relation of a therapist's patients
-    if users.all? {|user| user.obsessions.empty?} # If none of the therapist's patients have obsessions
-      content_tag(:small, "All of your patients are asymptomatic and nonobsessive!")
-    elsif users.patients_nonobsessive.empty? && users.patients_obsessive_but_symptomless.empty? # All of the therapist's patients have obsessions, and all of these obsessions have symptoms
-      content_tag(:small, "All of your OCD patients are physically symptomatic.")
-    elsif users.patients_nonobsessive.empty? && users.symptomatic.empty? # # All of the therapist's patients have obsessions, but none of these obsessions have symptoms
-      content_tag(:small, "None of your OCD patients is physically symptomatic.")
+  def present_patients(patients)
+    if current_user.therapist? && patients.all? {|patient| patient.counselor == current_user}
+      if patients.empty?
+        content_tag(:small, "0") + content_tag(:br)
+      else
+        render partial: "users_ul", locals: { users: patients }
+      end
     end
   end
 end
