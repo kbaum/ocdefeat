d54b6e7fa79f852fb5ec178edb73827aca41e6ef
github username <github email address>
2018-07-11 08:31:39 -0400

    change conquering to defeating, planning to designing plans

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -5,11 +5,11 @@ class UserDecorator < ApplicationDecorator
     helpers.pluralize user.send("#{string_object}s").count, "#{string_object}"
   end
 
-  def pronouns_for_conquering_ocd
-    current_user.patient? ? "You are currently conquering" : "The patient is currently conquering"
+  def pronouns_for_defeating_ocd
+    current_user.patient? ? "You are currently defeating" : "The patient is currently defeating"
   end
 
-  def pronouns_for_planning
+  def pronouns_for_designing_plans
     current_user.patient? ? "You designed" : "The patient designed"
   end
 end
