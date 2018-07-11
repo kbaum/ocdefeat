624c0216522f182eaadfb7dd42e2acb44b86e839
github username <github email address>
2018-07-11 09:43:03 -0400

    include Draper::LazyHelpers in UserDecorator; delete 'helpers'

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,16 +1,17 @@
 class UserDecorator < ApplicationDecorator
+  include Draper::LazyHelpers
   delegate_all
 
   def objects_count(string_object) # argument is a string like "obsession" or "plan"
-    helpers.pluralize user.send("#{string_object}s").count, "#{string_object}"
+    pluralize user.send("#{string_object}s").count, "#{string_object}"
   end
 
   def pronouns_for_defeating_ocd
-    helpers.current_user.patient? ? "You are currently defeating" : "The patient is currently defeating"
+    current_user.patient? ? "You are currently defeating" : "The patient is currently defeating"
   end
 
   def pronouns_for_designing_plans
-    helpers.current_user.patient? ? "You designed" : "The patient designed"
+    current_user.patient? ? "You designed" : "The patient designed"
   end
 
   def present_clinical_presentation
