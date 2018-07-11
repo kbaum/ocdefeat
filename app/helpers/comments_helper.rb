5f867a2f0f0518250c92892444cd0c34b2fa9eaf
github username <github email address>
2018-07-11 19:41:56 -0400

    delete #placefill_comment helper in CommentsHelper

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,12 +1,4 @@
 module CommentsHelper
-  def placefill_comment
-    if current_user.therapist?
-      "Add mental health tips..."
-    elsif current_user.patient?
-      "Feel free to vent here..."
-    end
-  end
-
   def label_submit_btn
     if current_user.therapist?
       "Share Your Advice"
