ea8c39f9fdb993ff74aebecd60bd584dacfcf169
github username <github email address>
2018-07-12 16:11:44 -0400

    define #delete_link in ApplicationDecorator

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -15,4 +15,10 @@ class ApplicationDecorator < Draper::Decorator
       content_tag(:span, nil, class: "glyphicon glyphicon-edit") + content_tag(:small, " Edit #{object.class}")
     end
   end
+
+  def delete_link
+    link_to polymorphic_path(object), method: :delete, data: { confirm: "Are you sure you want to delete this #{object.class.to_s.downcase}?" }, class: "btn btn-danger btn-sm" do
+      content_tag(:span, nil, class: "glyphicon glyphicon-trash") + content_tag(:small, "Delete #{object.class}")
+    end
+  end
 end
