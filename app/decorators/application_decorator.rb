d461443fc11b4bcfd794fef86af0c74799281770
github username <github email address>
2018-07-12 16:05:25 -0400

    delete object_type;change object_type to object.class

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -10,20 +10,9 @@ class ApplicationDecorator < Draper::Decorator
     updated_at.strftime("Last modified on %A, %B %e, %Y, at %l:%M %p")
   end
 
-  def object_type
-    case object.class.to_s
-    when "User"
-      "Account"
-    when "Plan"
-      "Plan Overview"
-    else
-      object.class.to_s
-    end
-  end
-
   def edit_link
     link_to polymorphic_path(object, action: "edit"), class: "btn btn-primary btn-sm" do
-      content_tag(:span, nil, class: "glyphicon glyphicon-edit") + content_tag(:small, " Edit #{object_type}")
+      content_tag(:span, nil, class: "glyphicon glyphicon-edit") + content_tag(:small, " Edit #{object.class}")
     end
   end
 end
