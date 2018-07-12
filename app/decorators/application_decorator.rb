95759837ce5bf2f884536a06cdf02779b775463b
github username <github email address>
2018-07-12 15:45:38 -0400

    define #object_type and call within #edit_link

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -10,9 +10,20 @@ class ApplicationDecorator < Draper::Decorator
     updated_at.strftime("Last modified on %A, %B %e, %Y, at %l:%M %p")
   end
 
+  def object_type
+    case object.class.to_s
+    when "User"
+      "Account"
+    when "Plan"
+      "Plan Overview"
+    else
+      object.class.to_s
+    end
+  end
+
   def edit_link
     link_to polymorphic_path(object, action: "edit"), class: "btn btn-primary btn-sm" do
-      content_tag(:span, nil, class: "glyphicon glyphicon-edit") + content_tag(:small, " Edit #{object.class}")
+      content_tag(:span, nil, class: "glyphicon glyphicon-edit") + content_tag(:small, " Edit #{object_type}")
     end
   end
 end
