bfe2423ba107c9ddda4cad8977fc1292e943e322
github username <github email address>
2018-07-11 20:19:43 -0400

    delete #obsessions_per_theme in Theme model

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -8,8 +8,4 @@ class Theme < ApplicationRecord
   def prevalence_in_patients # instance method returns number of unique users who have at least 1 obsession w/ theme content
     User.obsessing_about(self).count
   end
-
-  def obsessions_per_theme
-    obsessions.count
-  end
 end
