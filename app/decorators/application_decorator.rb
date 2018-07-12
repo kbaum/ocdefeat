55ac78b3c410c7b6b6fdd54828a35416fd75762d
github username <github email address>
2018-07-12 13:44:11 -0400

    define #edit_link to dynamically return edit link w/ edit glyphicon

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -1,12 +1,18 @@
 class ApplicationDecorator < Draper::Decorator
   include Draper::LazyHelpers
   delegate_all
-  
+
   def created_on
     created_at.strftime("%A, %B %e, %Y, at %l:%M %p")
   end
 
   def last_modified
     updated_at.strftime("Last modified on %A, %B %e, %Y, at %l:%M %p")
+  end
+
+  def edit_link
+    link_to polymorphic_path(object, action: "edit"), class: "btn btn-primary btn-sm" do
+      content_tag(:span, nil, class: "glyphicon glyphicon-edit") + content_tag(:small, "Edit #{object.class}")
+    end
   end
 end
