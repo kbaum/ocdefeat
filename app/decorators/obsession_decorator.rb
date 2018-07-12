508ae6cfdde976d2234d6a8d7518cb8475f17a28
github username <github email address>
2018-07-12 10:07:02 -0400

    delete delegate_all, inherit from Application::Decorator

--- app/decorators/obsession_decorator.rb
+++ app/decorators/obsession_decorator.rb
@@ -1,6 +1,4 @@
-class ObsessionDecorator < Draper::Decorator
-  delegate_all
-
+class ObsessionDecorator < Application::Decorator
   def hypotheticalize # sets intrusive_thought attribute of obsession = formatted string
     idea = intrusive_thought.downcase.split(/\A\bwhat\b\s+\bif\b\s+\bi\b\s+/).join("").split("?").join("")
     intrusive_thought = "What if I " << "#{idea}?"
