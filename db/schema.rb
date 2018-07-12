59f36bde6cca81bd4cbc2d9cc9256ae39a7ca4a0
github username <github email address>
2018-07-12 17:57:16 -0400

    rename description column definition in themes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180702181253) do
+ActiveRecord::Schema.define(version: 20180712215546) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -68,7 +68,7 @@ ActiveRecord::Schema.define(version: 20180702181253) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "description"
+    t.string "definition"
     t.integer "user_id"
   end
 
