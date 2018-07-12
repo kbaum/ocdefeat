ad42704aed63309c6cfe11db21f2031bdd3fe99f
github username <github email address>
2018-07-12 16:14:03 -0400

    add space before 'Delete' in #delete_link

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -18,7 +18,7 @@ class ApplicationDecorator < Draper::Decorator
 
   def delete_link
     link_to polymorphic_path(object), method: :delete, data: { confirm: "Are you sure you want to delete this #{object.class.to_s.downcase}?" }, class: "btn btn-danger btn-sm" do
-      content_tag(:span, nil, class: "glyphicon glyphicon-trash") + content_tag(:small, "Delete #{object.class}")
+      content_tag(:span, nil, class: "glyphicon glyphicon-trash") + content_tag(:small, " Delete #{object.class}")
     end
   end
 end
