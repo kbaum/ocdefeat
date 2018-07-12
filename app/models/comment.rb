8638962164f90fa7a7dc41f49486cc2becbe19ab
github username <github email address>
2018-07-12 15:24:27 -0400

    change prefix from true to 'commenter' in delegate :name

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,7 +7,7 @@ class Comment < ApplicationRecord
   belongs_to :obsession
   belongs_to :user
 
-  delegate :name, to: :user, prefix: true, allow_nil: true # calling #user_name on comment instance returns the name attribute value of the user instance to which the comment belongs.
+  delegate :name, to: :user, prefix: "commenter", allow_nil: true # calling #commenter_name on comment instance returns the name attribute value of the user instance to which the comment belongs.
 
   validates :content, presence: true
 end
