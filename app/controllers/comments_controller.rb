06a689a8aa3c37457d49d662b6ba794fb5224c8d
github username <github email address>
2018-07-13 09:49:08 -0400

    use policy_scope(Comment) and where(obsession: ...) in comments#index

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -35,7 +35,7 @@ class CommentsController < ApplicationController
 
   def index # Route helper #obsession_comments_path returns "/obsessions/:obsession_id/comments", which maps to comments#index
     @obsession = Obsession.find(params[:obsession_id]).decorate
-    @comments = @obsession.comments.decorate # stores all comments on a single obsession (concerns and advice)
+    @comments = policy_scope(Comment).where(obsession: @obsession).decorate # stores all comments on a single obsession (concerns and advice)
     patient_obsessing = @obsession.user # stores the patient who reported the obsession
     authorize patient_obsessing, :show_comments? # A patient can see all comments on her own obsessions. A therapist can see all comments on her own patients' obsessions.
 
