7c2f1e3c817b554d30c0a5a36bf40c7cf8393991
github username <github email address>
2018-07-15 19:28:21 -0400

    omit space; place flash.now[:error] below building treatments in #create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,19 +44,18 @@ class UsersController < ApplicationController
   end
 
   def new
-    @user = User.new
+    @user = User.new # instance for form_for to wrap around
     3.times { @user.treatments.build }
   end
 
   def create
     @user = User.new(user_params)
-
     if @user.save
       session[:user_id] = @user.id # log in the user
       redirect_to user_path(@user), flash: { success: "You successfully registered and created your preliminary profile, #{current_user.name}!" }
     else
-      flash.now[:error] = "Your registration attempt was unsuccessful. Please try again."
       3.times { @user.treatments.build }
+      flash.now[:error] = "Your registration attempt was unsuccessful. Please try again."
       render :new # present the registration form so the user can try signing up again
     end
   end
